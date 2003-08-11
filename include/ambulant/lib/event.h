ff4df24e75a2b2e721bb0d8943ce4bc5b4750ed0
anadelt <devnull@localhost>
2003-08-11 18:27:56 +0000

    Simplify timing related code. Remove timeout event class. It complicates things without any real benefit. To schedule an event we may pass the timeout value as a second argument.

--- include/ambulant/lib/event.h
+++ include/ambulant/lib/event.h
@@ -23,22 +23,6 @@ class event {
 	virtual void fire() = 0;
 };
 
-template <typename time_type = unsigned long>
-class timeout_event : public event {
-  public:
-	typedef time_type self_time_type;
-	
-	// Returns the relative time this event is scheduled to fire.
-	virtual self_time_type get_time() = 0;
-	
-	// Incr/decr relative schedule time.
-	virtual void incr(self_time_type dt) = 0;
-	virtual void decr(self_time_type dt) = 0;
-	
-	// Sets the relative time this event is scheduled to fire.
-	virtual void set_timeout(self_time_type t) = 0;
-};
- 
 } // namespace lib
  
 } // namespace ambulant
