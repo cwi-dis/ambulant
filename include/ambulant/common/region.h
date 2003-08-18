f33704521e6da7cd7b9d2fd8098a56c7ef9b93f2
jack <devnull@localhost>
2003-08-18 13:44:53 +0000

    Started on interface for active_region objects (renderers), and on the code in active_timeline to support them.
    Moved timeline boilerplate stuff (the xxx_TIMELINE events) to
    the timeline implementation in stead of doing it in the caller.
    
    Augmented the start() call with an optional callback event argument,
    to be fired when the timeline finishes.

--- include/ambulant/common/region.h
+++ include/ambulant/common/region.h
@@ -13,24 +13,50 @@
 #ifndef AMBULANT_LIB_REGION_H
 #define AMBULANT_LIB_REGION_H
 
+#include "ambulant/lib/node.h"
+#include "ambulant/lib/callback.h"
+#include "ambulant/lib/event_processor.h"
+
 namespace ambulant {
 
 namespace lib {
 
+// Forward
 class active_region;
 
 class passive_region {
   public:
-	passive_region() {}
+	friend class active_region;
+	
+	passive_region() 
+	:	m_name("unnamed") {}
+	passive_region(char *name)
+	:	m_name(name) {}
 	~passive_region() {}
 	
-	active_region *activate();
+	active_region *activate(event_processor *const evp, node *node);
+  private:
+  	char *m_name; // for debugging
 };
 
 class active_region {
   public:
-	active_region() {}
+	active_region(event_processor *const evp,
+		passive_region *const source,
+		node *node)
+	:	m_event_processor(evp),
+		m_source(source),
+		m_node(node) {}
 	~active_region() {}
+	
+	void start(event *playdone);
+	void stop();
+	
+
+  private:
+  	event_processor *const m_event_processor;
+	passive_region *const m_source;
+	node *m_node;
 };
 
 } // namespace lib
---
+++ src/libambulant/common/region.cpp
@@ -0,0 +1,40 @@
+/* 
+ * @$Id$ 
+ */
+
+/*
+ * The contents of this file are subject to the Public
+ * License XXX. 
+ *
+ */
+
+#include "ambulant/lib/region.h"
+
+namespace ambulant {
+
+namespace lib {
+
+
+active_region *
+passive_region::activate(event_processor *const evp, node *node)
+{
+	return new active_region(evp, this, node);
+}
+
+void
+active_region::start(event *playdone)
+{
+	std::cout << "active_region.start(" << m_source->m_name << ")" << std::endl;
+	if (playdone)
+		m_event_processor->add_event(playdone, 0, event_processor::low);
+}
+
+void
+active_region::stop()
+{
+	std::cout << "active_region.stop(" << m_source->m_name << ")" << std::endl;
+}
+
+} // namespace lib
+ 
+} // namespace ambulant
