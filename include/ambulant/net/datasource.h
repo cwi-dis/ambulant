1a451e5a0a769ec2b1d5ea562bea0c768c83af4d
dbenden <devnull@localhost>
2003-08-21 14:54:18 +0000

    active and passive datasources are now included, but untested.

--- include/ambulant/net/datasource.h
+++ include/ambulant/net/datasource.h
@@ -7,12 +7,16 @@
 #ifndef AMBULANT_NET_DATASOURCE_H
 #define AMBULANT_NET_DATASOURCE_H
 
-//#include "ambulant/lib/callback.h"
-
+#include "ambulant/lib/callback.h"
+#include "ambulant/lib/refcount.h"
+#include "ambulant/lib/event_processor.h"
+#include "ambulant/lib/unix/unix_mtsync.h"
+#include "ambulant/lib/unix/unix_event_processor.h"
 
 // temporary debug messages
 #include <iostream>
 #include <ostream>
+#include <fstream>
 #include <iomanip>
 #include <cstring>
 
@@ -24,61 +28,84 @@ namespace net {
 namespace datasource {
 	
 typedef char bytes; 
-typedef char url;
 
-
-class databuffer 
+class databuffer  
 {
  private: 
-	bytes *buffer; 				     // duh, pointer to the buffer.
-	int s;  																	 // the size of the bufer.
-	int used;																// howmany bytes are in the buffer 
+     // duh, pointer to the buffer.
+	char *m_buffer; 			
+		     
+	// the size of the bufer.
+	int m_size;  			
 	
-	void shift_down(int pos);										// shift down all data above pos.
-
+	// how many bytes are in the buffer 														 
+	int m_used;							
+										
+	// shift down all data above pos
+	void shift_down(int pos);										
  public:
 	// constructors
-	databuffer();				    // default constructor
-	databuffer(int size);			    // natural constructor
+	databuffer();				
+	databuffer(int size);	
 
-	databuffer(databuffer& buf);    	    // copy constructor
+	databuffer(databuffer& buf);    	  
 	
 	// destructor
 	~databuffer();
 	
 	void resize(int newsize);
 	void show(bool verbose);									// show information about the buffer, if verbose is true the buffer is dumped to cout;
-	void get_data(bytes *data, int size); 				//retrieve data from buffer,  still thinking about arguments.
+	void get_data(char *data, int size); 				//retrieve data from buffer,  still thinking about arguments.
 																				//there should be something to pass on error messages.
-	void put_data(bytes *data , int size);			 	// this one puts data alway at the end.						 
+	void put_data(char *data , int size);			 	// this one puts data alway at the end.						 
 	
+	int used();
 };
 
-class passive_datasource
+
+class active_datasource;
+
+class passive_datasource 
 {
-private:
-	url where;
-	
 public:
 	passive_datasource();
-	passive_datasource(url location);
-	passive_datasource(passive_datasource& ds);
+	passive_datasource(char *location);
+//	passive_datasource(passive_datasource& ds);
 	
 	~passive_datasource();
 	
-	void activate();
+	active_datasource *activate();
+	
+private:
+	char *m_url;
 };
 
-
-class active_datasource
-{
-private:
-	databuffer buffer();
-	
+ 
+class active_datasource  {  	
 public:
+	// constructors 
+	active_datasource();
+	active_datasource(passive_datasource *const source,std::ifstream &file);
+	 // destructor
+	~active_datasource();
 
-void start_read_all(url loc);
 
+	void start(ambulant::lib::unix::event_processor *evp,ambulant::lib::event *readdone);
+
+
+	//  Get data from buffer and put 'size' bytes in buffer.
+	void read(char *data, int size);
+
+	// Return the amount of data currently in buffer.
+	int size();
+
+private:
+    databuffer *buffer;
+    passive_datasource *m_source;
+	int m_filesize;
+	std::ifstream m_stream;
+	void filesize(std::ifstream &file);
+    void read_file(std::ifstream &file);
 
 };
 
