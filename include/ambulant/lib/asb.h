389d3de021ff1c30dff5a13e49557b017d1cd3b5
anadelt <devnull@localhost>
2003-08-29 09:53:33 +0000

    Give access through the namespace lib to unistd functions. Currently only sleep(secs).

---
+++ include/ambulant/lib/asb.h
@@ -0,0 +1,47 @@
+
+/* 
+ * @$Id$ 
+ */
+
+/*
+ * The contents of this file are subject to the Public
+ * License XXX. 
+ *
+ */
+
+// This header defines and exports  
+// a set of handy not std functions.
+// All functions defined here are accessible
+// through the lib namespace.
+// For example to invoke on unix or windows 
+// sleep(secs) use:
+// lib::sleep(secs); // within ambulant ns
+
+#ifndef AMBULANT_LIB_ASB_H
+#define AMBULANT_LIB_ASB_H
+
+#ifdef WIN32
+#include "ambulant/lib/win32/win32_asb.h"
+#else
+#include <unistd.h>
+#endif
+
+namespace ambulant {
+
+namespace lib {
+
+#ifdef WIN32
+
+using ambulant::lib::win32::sleep;
+
+#else
+
+using ::sleep;
+
+#endif
+
+} // namespace lib
+
+} // namespace ambulant
+
+#endif // AMBULANT_LIB_ASB_H
