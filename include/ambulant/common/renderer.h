60ce8d9a9c6605d25ebe7e45d68fba4c50f8ef56
jack <devnull@localhost>
2003-09-09 13:35:12 +0000

    We now actually create the data sources and let them pump data to the renderers. The renderers only print the size of the data, but at least all scheduling issues (well, some scheduling issues:-) have surfaced and been resolved.

--- include/ambulant/common/renderer.h
+++ include/ambulant/common/renderer.h
@@ -16,6 +16,7 @@
 #include "ambulant/lib/node.h"
 #include "ambulant/lib/region.h"
 #include "ambulant/lib/callback.h"
+#include "ambulant/lib/mtsync.h"
 #include "ambulant/lib/event_processor.h"
 #include "ambulant/net/datasource.h"
 
@@ -23,33 +24,64 @@ namespace ambulant {
 
 namespace lib {
 
-// Forward
+namespace detail {
 
-class active_renderer {
+class readdone_callback_arg {
+};
+
+}; // namespace detail
+
+class active_renderer : public ref_counted {
   public:
   	active_renderer()
   	:	m_event_processor(NULL),
   		m_src(NULL),
-  		m_node(NULL) {}
+  		m_node(NULL),
+  		m_readdone(NULL),
+  		m_playdone(NULL),
+  		m_refcount(1) {}
+  	active_renderer(const ambulant::lib::active_renderer& src)
+  	:	m_event_processor(src.m_event_processor),
+  		m_src(src.m_src),
+  		m_node(src.m_node),
+  		m_readdone(src.m_readdone),
+  		m_playdone(src.m_playdone),
+  		m_refcount(1) {}
 	active_renderer(event_processor *const evp,
 		net::passive_datasource *src,
 		passive_region *const dest,
-		const node *node)
-	:	m_event_processor(evp),
-		m_src(src?src->activate():NULL),
-		m_dest(dest->activate(evp, node)),
-		m_node(node) {}
+		const node *node);
+		
 	~active_renderer() {}
 	
 	void start(event *playdone);
 	void stop();
 	
+	////////////////////////
+	// lib::ref_counted interface implementation
+	
+	long add_ref() {return ++m_refcount;}
+
+	long release() {
+		if(--m_refcount == 0){
+			delete this;
+			return 0;
+		}
+		return m_refcount;
+	}
+
+	long get_ref_count() const {return m_refcount;}
 
   private:
+	void readdone(detail::readdone_callback_arg *dummy);
+
   	event_processor *const m_event_processor;
   	net::active_datasource *m_src;
 	active_region *const m_dest;
 	const node *m_node;
+	lib::event *m_readdone;
+	lib::event *m_playdone;
+	basic_atomic_count<critical_section> m_refcount;
 };
 
 } // namespace lib
--- src/libambulant/common/renderer.cpp
+++ src/libambulant/common/renderer.cpp
@@ -13,21 +13,54 @@
 
 using namespace ambulant;
 
+typedef lib::callback<lib::active_renderer,lib::detail::readdone_callback_arg> readdone_callback;
+
+lib::active_renderer::active_renderer(event_processor *const evp,
+	net::passive_datasource *src,
+	passive_region *const dest,
+	const node *node)
+:	m_event_processor(evp),
+	m_src(src?src->activate():NULL),
+	m_dest(dest->activate(evp, node)),
+	m_node(node),
+	m_readdone(NULL),
+	m_playdone(NULL),
+	m_refcount(1)
+{
+	m_readdone = new readdone_callback(this, &lib::active_renderer::readdone, 
+					new lib::detail::readdone_callback_arg());
+}
 
 void
 lib::active_renderer::start(lib::event *playdone)
 {
+	if (!m_node) abort();
+	m_playdone = playdone;
 	std::ostringstream os;
 	os << *m_node;
 	lib::logger::get_logger()->trace("active_renderer.start(0x%x, %s, playdone=0x%x)", (void *)this, os.str().c_str(), (void *)playdone);
 	m_dest->show();
-	if (playdone)
-		m_event_processor->add_event(playdone, 0, event_processor::low);
+	if (m_src) {
+		m_src->start(m_event_processor, m_readdone);
+	} else {
+		lib::logger::get_logger()->trace("active_renderer.start: no datasource");
+		if (m_playdone)
+			m_event_processor->add_event(m_playdone, 0, event_processor::low);
+	}
+}
+
+void
+lib::active_renderer::readdone(lib::detail::readdone_callback_arg *dummy)
+{
+	lib::logger::get_logger()->trace("active_renderer.readdone(0x%x, size=%d)", (void *)this, m_src->size());
+	if (m_playdone)
+		m_event_processor->add_event(m_playdone, 0, event_processor::low);
 }
 
 void
 lib::active_renderer::stop()
 {
+	// XXXX Need to handle case that no data (or not all data) has come in yet
 	m_dest->done();
 	lib::logger::get_logger()->trace("active_renderer.stop(0x%x)", (void *)this);
 }
