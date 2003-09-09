29c2ad1d5cfb0fb48e39563e1c40b7af420a0045
anadelt <devnull@localhost>
2003-09-09 20:35:37 +0000

    Even though was left rather difficult to implement ref counted objects, a not negligible portion of the code uses ref counting. As a consequence repeats again and again the same boiler plate code. Implement a class (ref_counted_obj) that willl simplify all refcounted objects. Just change ref_counted to ref_counted_obj and remove all the code related with ref counting. Simplify this module by removing support for non self destroying objects since natural selection decided so.

--- include/ambulant/lib/refcount.h
+++ include/ambulant/lib/refcount.h
@@ -16,9 +16,15 @@
 #ifndef AMBULANT_LIB_REFCOUNT_H
 #define AMBULANT_LIB_REFCOUNT_H
 
+// assert
+#include <cassert>
+
+#include "ambulant/lib/mtsync.h"
+
 namespace ambulant {
 
 namespace lib {
+
 
 // Interface of reference counted objects.
 class ref_counted {
@@ -29,30 +35,9 @@ class ref_counted {
 	virtual long get_ref_count() const = 0;
 };
 
-// Release function for not self-destroying objects.
-// Usage idiom:
-// ptr_to_ref_counted = lib::release(ptr_to_ref_counted);
-template <class T>
-static T* release(T *p) {
-	p->release();
-	if(p->get_ref_count() == 0) {
-		delete p;
-		return 0;
-	}
-	return p;
-}
-
-
-class null_critical_section {
-  public:
-	void enter() {}
-	void leave() {}
-};
-
-
 // Template argument T exposes a critical section interface.
 // Boost libraries have a similar construct we may choose to use at some point.
-template <class T = null_critical_section >
+template <class T>
 class basic_atomic_count {	
   public:
 	explicit basic_atomic_count(long v): m_value(v){}
@@ -80,23 +65,25 @@ class basic_atomic_count {
 	T m_cs;
 };
 
+typedef basic_atomic_count<critical_section> atomic_count;
 
-typedef basic_atomic_count<null_critical_section> atomic_count;
 
-
-// This class is useful by itself as an envelope 
-// controlling the lifetime of its content,
-// but can serve also as a sample for ref_counted classes.
-// This is a self-destroying version.
-// See next one for a not self-destroying version.
-// Both adhere to a well defined (but diff) usage contract.
-template<class T>
-class sd_envelope : public ref_counted {
+// The following class maybe used as a mixin for classes
+// implementing ref_counted objects.
+// For example:
+// class active_player : public ref_counted_obj {
+//		....
+//		No code related with ref counting.
+//		Even the m_refcount doesn't have
+//		to be initialized in the constructor of active_player 
+//		since the base ref_counted_obj does this by default.
+//		....
+// };
+class ref_counted_obj : public ref_counted {
   public:
-	sd_envelope(T* value) : m_value(value), m_refcount(1){}
+	ref_counted_obj()
+	:	m_refcount(1) {}
 	
-	T* get_value() { return m_value;}
-
 	long add_ref() {return ++m_refcount;}
 
 	long release() {
@@ -106,37 +93,36 @@ class sd_envelope : public ref_counted {
 		}
 		return m_refcount;
 	}
-
 	long get_ref_count() const {return m_refcount;}
+
   protected:
-	// force client to use release
-	~sd_envelope() { delete m_value;}
-	
-  private:
-	T* m_value;
 	atomic_count m_refcount;
 };
 
+
+// This class is useful by itself as an envelope 
+// controlling the lifetime of its content,
+// but can serve also as a sample for ref_counted classes.
+// This is the self-destroying version of ref counted objects.
 template<class T>
-class envelope : public ref_counted {
+class sd_envelope : public ref_counted_obj {
   public:
-	envelope(T* value) : m_value(value), m_refcount(1){}
-	
-	~envelope() { delete m_value;}
-	
+	sd_envelope(T* value) : m_value(value) {}
+	~sd_envelope() { delete m_value;}
 	T* get_value() { return m_value;}
-
-	long add_ref() { return ++m_refcount;}
-
-	long release() { return --m_refcount;}
-
-	long get_ref_count() const { return m_refcount;}
-	
   private:
 	T* m_value;
 	atomic_count m_refcount;
 };
 
+// Save release idiom: 
+// p = release(p); 
+// if p is deleted then p == 0.
+template <class T>
+static T* release(T *p) {
+	if(!p) return p;
+	return p->release()?p:0;
+}
 
 } // namespace lib
  
