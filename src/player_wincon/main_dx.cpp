c9275e5508302f522bc2f33f0560e239ba041faf
anadelt <devnull@localhost>
2003-10-15 09:28:19 +0000

    Utilize the latest dx interface.

--- src/player_wincon/main_dx.cpp
+++ src/player_wincon/main_dx.cpp
@@ -5,13 +5,11 @@
 
 #include "ambulant/version.h"
 
-#include "ambulant/lib/player.h"
-#include "ambulant/lib/event.h"
-#include "ambulant/lib/event_processor.h"
-#include "ambulant/lib/asb.h"
+#include "ambulant/gui/dx/dx_player.h"
 #include "ambulant/lib/logger.h"
+#include "ambulant/lib/asb.h"
 
-#include "ambulant/gui/dx/dx_gui.h"
+#include <iostream>
 
 #pragma comment (lib,"libjpeg.lib")
 #pragma comment (lib,"libexpat.lib")
@@ -26,58 +24,26 @@ main(int argc, char **argv) {
 		std::cerr << "Usage: player_wincon file" << std::endl;
 		return 0;
 	}
-	char *filename = argv[1];
-	
+	std::string filename =  lib::win32::resolve_path(argv[1]);
 	lib::logger *logger = lib::logger::get_logger();
-	logger->trace("Play: %s", filename);
 	
 	if(FAILED(CoInitialize(NULL))) {
 		logger->error("CoInitialize() failed");
 		return 1;
 	}
 	
-	// Create passive_player from filename
-	lib::passive_player *pplayer = new lib::passive_player(filename);
-	if (!pplayer) {
-		logger->error("Failed to construct passive_player from file %s", filename);
+	gui::dx::dx_player *player = gui::dx::dx_player::create_player(filename);	
+	if(!player->start()) {
+		logger->error("Player failed to start playback");
 		return 1;
 	}
-	
-	// Create GUI window_factory and renderer_factory
-	lib::window_factory *wf = new gui::dx::dx_window_factory();
-	lib::renderer_factory *rf = new ambulant::gui::dx::dx_renderer_factory();
-	
-	// Request an active_player for the provided factories 
-	lib::active_player *aplayer = pplayer->activate(wf, rf);
-	if (!aplayer) {
-		logger->error("passive_player::activate() failed to create active_player");
-		return 1;
-	}
-	
-	// Create an event_processor
-	lib::event_processor *processor = 
-		lib::event_processor_factory(lib::realtime_timer_factory());
-	
-	// Start event_processor. 
-	// Pass a flag_event to be set when done.
-	bool done = false;
-	logger->trace("Start playing");
-
-	aplayer->start(processor, new lib::flag_event(done));
-	while(!done) {
+	lib::sleep_msec(500);
+	while(!player->is_done()) {
 		lib::sleep_msec(500);
 	}
-	logger->trace("Finished playing");
+	logger->trace("Stoped playing");
+	delete player;
 	
-	// cleanup
-	delete processor;
-	delete pplayer;
-	aplayer = lib::release(aplayer);
-	// verify:
-	if(aplayer != 0)
-		logger->warn("active_player ref_count: " + aplayer->get_ref_count());
-	delete rf;
-	delete wf;
 	CoUninitialize();	
 	logger->trace("Normal exit");
 	return 0;
