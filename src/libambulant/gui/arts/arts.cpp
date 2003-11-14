76562e2f1366064a476f8f3e05cb8c2d577b620c
Daniel Benden <koelimoe@sf.net>
2003-11-14 15:51:49 +0000

    more code, no interessting changes

--- src/libambulant/gui/arts/arts.cpp
+++ src/libambulant/gui/arts/arts.cpp
@@ -45,12 +45,48 @@
  * which carries forward this exception.
  *
  */
+
+ 
+#ifndef AM_DBG
+#define AM_DBG if(0)
+#endif
+ 
 #include "ambulant/gui/arts/arts.h"
 
 using namespace ambulant;
 
+lib::active_renderer *
+gui::arts::arts_renderer_factory::new_renderer(lib::event_processor *const evp,
+	net::passive_datasource *src,
+	lib::passive_region *const dest,
+	const lib::node *node)
+{
+	active_renderer *rv;
+
+	xml_string tag = node->get_qname().second;
+	if ( tag == "audio") {
+		rv = (active_renderer *)new arts_active_audio_renderer(evp, src, node);
+		AM_DBG logger::get_logger()->trace("cocoa_renderer_factory: node 0x%x: returning cocoa_active_audio_renderer 0x%x", (void *)node, (void *)rv);
+	} else {
+	// logger::get_logger()->error("arts_renderer_factory: no aRts renderer for tag \"%s\"", tag.c_str());
+                return NULL;
+	}
+	return rv;
+}
+
+gui::arts::arts_active_audio_renderer::arts_active_audio_renderer(event_processor *const evp,
+	net::passive_datasource *src,
+	const node *node)
+:	active_basic_renderer(evp, node),
+	m_url(src->get_url())
+{
+    m_evp = evp;
+    m_src = src;
+    m_node = node;
+}
+
 int
-gui::arts_renderer::arts_active_renderer::arts_setup(int rate, int bits, int channels, char *name)
+gui::arts::arts_active_audio_renderer::arts_setup(int rate, int bits, int channels, char *name)
 {
     int err;
 
@@ -64,20 +100,59 @@ gui::arts_renderer::arts_active_renderer::arts_setup(int rate, int bits, int cha
 }
 
 
-gui::arts_renderer::arts_active_renderer::~arts_active_renderer()
+gui::arts::arts_active_audio_renderer::~arts_active_audio_renderer()
 {
     arts_close_stream(m_stream);
     arts_free();
 }
 
 int
-gui::arts_renderer::arts_active_renderer::arts_play(char *data, int size)
+gui::arts::arts_active_audio_renderer::arts_play(char *data, int size)
 {
     return ::arts_write(m_stream, data, size);
 }
 
+void
+lib::arts_active_audio_renderer::readdone()
+{
+	AM_DBG lib::logger::get_logger()->trace("active_renderer.readdone(0x%x)", (void *)this);
+    size=m_ads->size();
+    data = new char[data];
+    m_ads->read(data,size);
+    arts_setup()
+    arts_play(data,size);
+stopped_callback();
+}
 
+void
+lib::arts_active_audio_renderer::playdone()
+{
+	AM_DBG lib::logger::get_logger()->trace("active_renderer.playdone(0x%x)", (void *)this);
+    
+	stopped_callback();
+}
 
+void
+lib::arts_active_audio_renderer::start(double where)
+{
+    char *data;
+	if (!m_node) abort();
+	m_playdone = playdone;
+	std::ostringstream os;
+	os << *m_node;
+    
+	AM_DBG lib::logger::get_logger()->trace("active_renderer.start(0x%x, %s, playdone=0x%x)", (void *)this, os.str().c_str(), (void *)playdone);
+	m_dest->show(this);
+	if (m_ads) {
+		m_ads->start(m_event_processor, m_readdone);    
+	} else {
+		lib::logger::get_logger()->error("active_renderer.start: no datasource");
+		if (m_playdone)
+			m_event_processor->add_event(m_playdone, 0, event_processor::low);
+            stopped_callback();
+	}
+    
+}
 
 
 
