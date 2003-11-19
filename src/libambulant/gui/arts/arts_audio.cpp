2a723d6231df00807e34e724f31826f34dc37a23
Daniel Benden <koelimoe@sf.net>
2003-11-19 15:41:41 +0000

    After the split arts_audio.cpp contains the code for  the arts_active_audio_renderer

---
+++ src/libambulant/gui/arts/arts_audio.cpp
@@ -0,0 +1,139 @@
+/*
+ *
+ * This file is part of Ambulant Player, www.ambulantplayer.org.
+ *
+ * Copyright (C) 2003 Stiching CWI,
+ * Kruislaan 413, 1098 SJ Amsterdam, The Netherlands.
+ *
+ * Ambulant Player is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Ambulant Player is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ambulant Player; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * In addition, as a special exception, if you link Ambulant Player with
+ * other files to produce an executable, this library does not by itself
+ * cause the resulting executable to be covered by the GNU General Public
+ * License. This exception does not however invalidate any other reason why
+ * the executable file might be covered by the GNU General Public License.
+ *
+ * As a special exception, the copyright holders of Ambulant Player give
+ * you permission to link Ambulant Player with independent modules that
+ * communicate with Ambulant Player solely through the region and renderer
+ * interfaces, regardless of the license terms of these independent
+ * modules, and to copy and distribute the resulting combined work under
+ * terms of your choice, provided that every copy of the combined work is
+ * accompanied by a complete copy of the source code of Ambulant Player
+ * (the version of Ambulant Player used to produce the combined work),
+ * being distributed under the terms of the GNU General Public License plus
+ * this exception.  An independent module is a module which is not derived
+ * from or based on Ambulant Player.
+ *
+ * Note that people who make modified versions of Ambulant Player are not
+ * obligated to grant this special exception for their modified versions;
+ * it is their choice whether to do so.  The GNU General Public License
+ * gives permission to release a modified version without this exception;
+ * this exception also makes it possible to release a modified version
+ * which carries forward this exception.
+ *
+ */
+
+
+#ifndef AM_DBG
+#define AM_DBG if(0)
+#endif
+
+#include "ambulant/gui/arts/arts_audio.h"
+
+namespace ambulant {
+
+using namespace lib;
+gui::arts::arts_active_audio_renderer::arts_active_audio_renderer(
+	active_playable_events *context,
+	active_playable_events::cookie_type cookie,
+	const node *node,
+	event_processor *const evp,
+	net::passive_datasource *src)
+:	active_renderer(context, cookie, node, evp, src, NULL)
+{
+
+}
+
+int
+gui::arts::arts_active_audio_renderer::arts_setup(int rate, int bits, int channels, char *name)
+{
+    int err;
+    if (!m_stream) {
+    err = arts_init();
+    if (err < 0) {
+        return err;
+    }
+
+    m_stream = arts_play_stream(rate, bits, channels, name);
+    return 0;
+    }
+}
+
+
+
+gui::arts::arts_active_audio_renderer::~arts_active_audio_renderer()
+{
+    arts_close_stream(m_stream);
+    arts_free();
+}
+
+int
+gui::arts::arts_active_audio_renderer::arts_play(char *data, int size)
+{
+    return ::arts_write(m_stream, data, size);
+}
+
+void
+gui::arts::arts_active_audio_renderer::readdone()
+{
+    char *data;
+    int size;
+    AM_DBG lib::logger::get_logger()->trace("active_renderer.readdone(0x%x)", (void *)this);
+    size = m_src->size();
+    data = new char[size];
+    m_src->read(data,size);
+    arts_setup(44100,16,2,"arts_audio");
+    arts_play(data,size);
+    stopped_callback();
+}
+
+
+
+void
+gui::arts::arts_active_audio_renderer::start(double where)
+{
+
+    if (!m_node) abort();
+
+	std::ostringstream os;
+	os << *m_node;
+
+	AM_DBG lib::logger::get_logger()->trace("arts_active_audio_renderer.start(0x%x, %s)", (void *)this, os.str().c_str());
+	if (m_src) {
+		m_src->start(m_event_processor, m_readdone);
+	} else {
+		lib::logger::get_logger()->error("active_renderer.start: no datasource");
+		if (m_playdone) {
+            stopped_callback();
+        }
+	}
+
+}
+
+}// end namespace ambulant
+
+
+
