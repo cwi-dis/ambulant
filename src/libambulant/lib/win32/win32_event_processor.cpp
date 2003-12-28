30281c30a0ec3e4c66a48207c1ff6d506cc9ecb7
Kleanthis Kleanthous <kleanthis@sf.net>
2003-12-28 16:44:46 +0000

    Initialize COM libs for DX player.

--- src/libambulant/lib/win32/win32_event_processor.cpp
+++ src/libambulant/lib/win32/win32_event_processor.cpp
@@ -77,10 +77,12 @@ unsigned long
 lib::win32::event_processor::run() {
 	lib::logger* logger = lib::logger::get_logger();
 	logger->trace("event_processor::run(=0x%x)", this);
+	CoInitialize(NULL);
 	while(!exit_requested()) {	
 		serve_events();		
 		wait_event();
 	}
+	CoUninitialize();
 	logger->trace("event_processor::~run(=0x%x)", this);
 	return 0;
 }
