ad46235fca352e7a6039642f12b4c2448b79ea4b
Kleanthis Kleanthous <kleanthis@sf.net>
2004-01-20 09:09:01 +0000

    Fix some crashes so that we can continue development. abstract_smil_region_info may be null so protect the code against the fact.

--- src/libambulant/common/region.cpp
+++ src/libambulant/common/region.cpp
@@ -54,6 +54,7 @@
 #include "ambulant/common/region.h"
 #include "ambulant/common/renderer.h"
 
+#define AM_DBG
 #ifndef AM_DBG
 #define AM_DBG if(0)
 #endif
@@ -204,7 +205,7 @@ lib::passive_region::mouse_region_changed()
 }
 
 lib::passive_root_layout::passive_root_layout(const std::string &name, size bounds, window_factory *wf)
-:   passive_region(name, NULL, screen_rect<int>(point(0, 0), size(bounds.w, bounds.h)), point(0, 0), NULL)
+:   passive_region(name, NULL, screen_rect<int>(point(0, 0), bounds), point(0, 0), NULL)
 {
 	m_mouse_region = wf->new_mouse_region();
 	m_gui_window = wf->new_window(name, bounds, this);
@@ -212,10 +213,10 @@ lib::passive_root_layout::passive_root_layout(const std::string &name, size boun
 }
 		
 lib::passive_root_layout::passive_root_layout(const abstract_smil_region_info *info, size bounds, window_factory *wf)
-:   passive_region(info->get_name(), NULL, screen_rect<int>(point(0, 0), size(bounds.w, bounds.h)), point(0, 0), info)
+:   passive_region(info?info->get_name():"topLayout", NULL, screen_rect<int>(point(0, 0), bounds), point(0, 0), info)
 {
 	m_mouse_region = wf->new_mouse_region();
-	m_gui_window = wf->new_window(info->get_name(), bounds, this);
+	m_gui_window = wf->new_window((info?info->get_name():"topLayout"), bounds, this);
 	AM_DBG lib::logger::get_logger()->trace("passive_root_layout(0x%x, \"%s\"): window=0x%x, mouse_region=0x%x", (void *)this, m_name.c_str(), (void *)m_gui_window, (void *)m_mouse_region);
 }
 		
