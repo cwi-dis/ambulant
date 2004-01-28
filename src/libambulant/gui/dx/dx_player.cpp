134bf5b97b4f68b6a3d63bf030e0dbf3fab5584f
Jack Jansen <Jack.Jansen@cwi.nl>
2004-01-28 14:56:16 +0000

    Adapted for new mms_player

--- src/libambulant/gui/dx/dx_player.cpp
+++ src/libambulant/gui/dx/dx_player.cpp
@@ -53,7 +53,6 @@
  
 #include "ambulant/gui/dx/dx_player_impl.h"
 
-#include "ambulant/common/player.h"
 #include "ambulant/lib/event.h"
 #include "ambulant/lib/event_processor.h"
 #include "ambulant/lib/asb.h"
@@ -61,36 +60,33 @@
 #include "ambulant/lib/document.h"
 
 #include "ambulant/common/smil_player.h"
+#include "ambulant/common/mms_player.h"
 
 #include "ambulant/gui/dx/dx_gui.h"
 #include "ambulant/gui/dx/dx_viewport.h"
 
 using namespace ambulant;
 
-gui::dx::dx_player_impl::dx_player_impl(const std::string& url, VCF f) 
+gui::dx::dx_mms_player_impl::dx_mms_player_impl(const std::string& url, VCF f) 
 :	m_url(url),
 	m_create_viewport_fn(f),
 	m_viewport(0),
 	m_wf(0), 
 	m_rf(0),
-	m_pplayer(0), 
-	m_aplayer(0),
+	m_mms_player(0), 
 	m_logger(lib::logger::get_logger()) {
 		
 }
 
-gui::dx::dx_player_impl::~dx_player_impl() {
-	m_aplayer = lib::release(m_aplayer);
+gui::dx::dx_mms_player_impl::~dx_mms_player_impl() {
 	// verify:
-	if(m_aplayer != 0)
-		m_logger->warn("active_player ref_count: %ld" + m_aplayer->get_ref_count());
-	delete m_pplayer;
+	delete m_mms_player;
 	delete m_rf;
 	delete m_wf;
 	delete m_viewport;
 }
 
-gui::dx::viewport* gui::dx::dx_player_impl::create_viewport(int w, int h) {
+gui::dx::viewport* gui::dx::dx_mms_player_impl::create_viewport(int w, int h) {
 	if(m_create_viewport_fn)
 		return (*m_create_viewport_fn)(w, h);
 	if(!m_viewport)
@@ -98,17 +94,16 @@ gui::dx::viewport* gui::dx::dx_player_impl::create_viewport(int w, int h) {
 	return m_viewport;
 }
 
-bool gui::dx::dx_player_impl::is_done() const { 
-	return m_aplayer && m_aplayer->is_done();
+bool gui::dx::dx_mms_player_impl::is_done() const { 
+	return m_mms_player == NULL || m_mms_player->is_done();
 }
 
-bool gui::dx::dx_player_impl::start() {
+bool gui::dx::dx_mms_player_impl::start() {
 	m_logger->trace("Attempting to play: %s", m_url.c_str());
 
-	// Create passive_player from filename
-	m_pplayer = new lib::passive_player(m_url.c_str());
-	if (!m_pplayer) {
-		m_logger->error("Failed to construct passive_player from file %s", m_url.c_str());
+	lib::document *doc = lib::document::create_from_file(m_url);
+	if(!doc) {
+		m_logger->error("Failed to parse document %s", m_url.c_str());
 		return false;
 	}
 	
@@ -116,46 +111,40 @@ bool gui::dx::dx_player_impl::start() {
 	m_wf = new gui::dx::dx_window_factory(this);
 	m_rf = new gui::dx::dx_renderer_factory(this);
 	
-	// Request an active_player for the provided factories 
-	m_aplayer = m_pplayer->activate(m_wf, m_rf);
-	if (!m_aplayer) {
-		m_logger->error("passive_player::activate() failed to create active_player");
-		return false;
-	}
-	
-	// Pass a flag_event to be set when done.
-	m_aplayer->start();
+	m_mms_player = new lib::mms_player(doc, m_wf, m_rf);
+	m_mms_player->start();
+
 	m_logger->trace("Started playing");
 	return  true;
 }
 
-void gui::dx::dx_player_impl::stop() {
+void gui::dx::dx_mms_player_impl::stop() {
 	m_logger->trace("Attempting to stop: %s", m_url.c_str());
-	if(m_aplayer) m_aplayer->stop();
+	if(m_mms_player) m_mms_player->stop();
 	if(m_viewport) m_viewport->redraw();
 }
 
-void gui::dx::dx_player_impl::pause() {
+void gui::dx::dx_mms_player_impl::pause() {
 	m_logger->trace("Attempting to pause: %s", m_url.c_str());
-	if(m_aplayer)
-		m_aplayer->pause();
+	if(m_mms_player)
+		m_mms_player->pause();
 }
 
-void gui::dx::dx_player_impl::resume() {
+void gui::dx::dx_mms_player_impl::resume() {
 	m_logger->trace("Attempting to resume: %s", m_url.c_str());
-	if(m_aplayer)
-		m_aplayer->resume();
+	if(m_mms_player)
+		m_mms_player->resume();
 }
 
 // static 
 gui::dx::dx_player* gui::dx::dx_player::create_player(const std::string& url) {
-	//return new dx_player_impl(url, 0);
+	//return new dx_mms_player_impl(url, 0);
 	return new dx_smil_player_impl(url, 0);
 }
 
 // static 
 gui::dx::dx_player* gui::dx::dx_player::create_player(const std::string& url, VCF f) {
-	//return new dx_player_impl(url, f);
+	//return new dx_mms_player_impl(url, f);
 	return new dx_smil_player_impl(url, f);
 }
 
