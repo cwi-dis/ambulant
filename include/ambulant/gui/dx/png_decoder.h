a0a257d92b3b22d50103a31a3f388d3ede273b85
Kleanthis Kleanthous <kleanthis@sf.net>
2004-03-30 18:55:00 +0000

    Provide support for PNG images.

--- include/ambulant/gui/dx/png_decoder.h
+++ include/ambulant/gui/dx/png_decoder.h
@@ -53,14 +53,22 @@
 #ifndef AMBULANT_GUI_PNG_DECODER_H
 #define AMBULANT_GUI_PNG_DECODER_H
 
+#pragma comment (lib,"libpng13.lib")
+#pragma comment (lib,"zlib.lib")
+
 #ifndef _WINDOWS_
 #include <windows.h>
 #endif
 
+#include "png.h"
+
 #include <cassert>
 
-#include "ambulant/config/config.h"
+#ifndef AM_DBG
+#define AM_DBG if(0)
+#endif
 
+#include "ambulant/config/config.h"
 #include "ambulant/gui/dx/img_decoder.h"
 #include "ambulant/lib/logger.h"
 
@@ -77,6 +85,7 @@ class png_decoder : public img_decoder<DataSource, ColorType> {
 	virtual ~png_decoder();
 	virtual bool can_decode();
 	virtual dib_surface<ColorType>* decode();
+	static void png_read_mem_data(png_structp png_ptr, png_bytep data, png_uint_32 length);
   private:
 	lib::logger *m_logger;
 };
@@ -89,6 +98,16 @@ png_decoder<DataSource, ColorType>::png_decoder(DataSource* src, HDC hdc)
 
 template <class DataSource, class ColorType>
 png_decoder<DataSource, ColorType>::~png_decoder() {
+}
+
+template <class DataSource, class ColorType>
+void png_decoder<DataSource, ColorType>::png_read_mem_data(png_structp png_ptr, 
+	png_bytep data, png_uint_32 length) {
+	DataSource *src = (DataSource*)png_ptr->io_ptr;
+	DataSource::size_type n = src->read(data, length);
+	if(n != DataSource::size_type(length)) {
+      png_error(png_ptr, "Read Error");
+	}
 }
 
 template <class DataSource, class ColorType>
@@ -109,10 +128,90 @@ inline bool png_decoder<DataSource, ColorType>::can_decode() {
 template <class DataSource, class ColorType>
 inline dib_surface<ColorType>* 
 png_decoder<DataSource, ColorType>::decode() {
-	HMODULE hDLL = LoadLibrary("libpng.dll");
-	if(!hDLL) return NULL;
-	// use lib for decoding
-	return NULL;
+    png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL,
+      (png_error_ptr)NULL, (png_error_ptr)NULL);
+    if(!png_ptr) {
+		m_logger->error("png_create_read_struct() failed");
+        return 0;
+    }
+
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    if(!info_ptr) {
+        png_destroy_read_struct(&png_ptr, NULL, NULL);
+		m_logger->error("png_create_info_struct() failed");
+        return 0;
+    }
+	
+	m_src->seekg(8);
+	png_set_read_fn(png_ptr, (png_voidp)m_src, (png_rw_ptr)png_read_mem_data);	
+    png_set_sig_bytes(png_ptr, 8);
+    png_read_info(png_ptr, info_ptr);
+    
+    // get width, height, bit-depth and color-type
+	png_uint_32 width, height;
+	int depth, clrtype;
+    png_get_IHDR(png_ptr, info_ptr, &width, &height, &depth, &clrtype, NULL, NULL, NULL);
+	AM_DBG m_logger->trace("PNG: %dx%d %dbpp [clrtype:%d]", width, height, depth, clrtype);
+	
+	// expand images of all color-type and bit-depth to 3x8 bit RGB images
+	// let the library process things like alpha, transparency, background
+	if(depth == 16) png_set_strip_16(png_ptr);
+	if(clrtype == PNG_COLOR_TYPE_PALETTE) png_set_expand(png_ptr);
+	if(depth < 8) png_set_expand(png_ptr);
+	if(png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS)) png_set_expand(png_ptr);
+	if(clrtype == PNG_COLOR_TYPE_GRAY || clrtype == PNG_COLOR_TYPE_GRAY_ALPHA)
+		png_set_gray_to_rgb(png_ptr);
+        
+	// set the background color to draw transparent and alpha images over.
+	png_color_16 *pbgr;
+	png_color bgrclr;
+	bool has_bgr = false;
+	if(png_get_bKGD(png_ptr, info_ptr, &pbgr)) {
+		png_set_background(png_ptr, pbgr, PNG_BACKGROUND_GAMMA_FILE, 1, 1.0);
+		bgrclr.red   = (byte) pbgr->red;
+		bgrclr.green = (byte) pbgr->green;
+		bgrclr.blue  = (byte) pbgr->blue;
+		has_bgr = true;
+	}
+        
+	// if required set gamma conversion
+	double gamma;
+	if(png_get_gAMA(png_ptr, info_ptr, &gamma))
+		png_set_gamma(png_ptr, (double) 2.2, gamma);
+	
+	// after the transformations have been registered update info_ptr data
+	png_read_update_info(png_ptr, info_ptr);
+        
+	// get again width, height and the new bit-depth and color-type
+    png_get_IHDR(png_ptr, info_ptr, &width, &height, &depth, &clrtype, NULL, NULL, NULL);
+	m_logger->trace("PNG: %dx%d %d bpp [clrtype: %d]", width, height, depth, clrtype);
+	
+	// row_bytes = width x channels
+	png_uint_32 row_bytes = png_get_rowbytes(png_ptr, info_ptr);
+    png_uint_32 channels = png_get_channels(png_ptr, info_ptr);
+    assert(row_bytes == width * channels);
+	AM_DBG m_logger->trace("PNG: row_bytes = %d, channels=%d", row_bytes, channels);
+		
+	// create a bmp surface
+	ColorType *pBits = NULL;
+	BITMAPINFO *pbmpi = get_bmp_info(width, height, ColorType::get_bits_size());
+	HBITMAP bmp = CreateDIBSection(m_hdc, pbmpi, DIB_RGB_COLORS, (void**)&pBits, NULL, 0);
+	if(bmp==NULL || pBits==NULL) {
+		m_logger->error("CreateDIBSection() failed");
+		return 0;
+	}
+	surface<ColorType> *psurf = 
+		new surface<ColorType>(width, height, ColorType::get_bits_size(), pBits);
+	
+	png_bytep *row_ptrs = new png_bytep[height];
+	for(png_uint_32 i=0;i<height;i++)
+		row_ptrs[i] = (png_bytep) psurf->get_row(i);
+	png_read_image(png_ptr, row_ptrs);	
+	png_read_end(png_ptr, NULL); 
+	delete[] row_ptrs;
+    png_destroy_read_struct(&png_ptr, NULL, NULL);
+    	
+    return new dib_surface<ColorType>(bmp, psurf);
 }
 	
 } // namespace dx
