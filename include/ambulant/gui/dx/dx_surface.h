2430f6aa5409b437b595cc74f704fb15263a12bc
Kleanthis Kleanthous <kleanthis@sf.net>
2004-03-31 07:01:09 +0000

    Add a function so that we are able to reverse byte order inline (e.g. avoiding allocating a second buffer for that).

--- include/ambulant/gui/dx/dx_surface.h
+++ include/ambulant/gui/dx/dx_surface.h
@@ -81,7 +81,7 @@ class surface {
 	typedef T& reference;
 	typedef const T& const_reference;
 
-	surface(size_t width, size_t height, size_t depth, pointer data)
+	surface(int width, int height, int depth, pointer data)
 	:	m_width(width), 
 		m_height(height), 
 		m_depth(depth), 
@@ -95,40 +95,40 @@ class surface {
 		uchar_ptr pb = uchar_ptr(m_data);
 		for (int y=m_height-1;y>=0;y--) {
 			pointer ptr = pointer(pb);
-			for(size_t x=0;x<m_width;x++)
+			for(int x=0;x<m_width;x++)
 				*ptr++ = bkcolor;
 			pb += m_pitch;
 		}
 	}
 
-	reference pixel(size_t x, size_t y) { 
+	reference pixel(int x, int y) { 
 		if(x>=m_width || y>=m_height) throw_range_error();
 		uchar_ptr pb = uchar_ptr(m_data) + (m_height - 1 - y)*m_pitch;
 		pointer ptr = pointer(pb);
 		return  ptr[x];
 	}
 
-	const_reference pixel(size_t x, size_t y) const { 
+	const_reference pixel(int x, int y) const { 
 		return pixel(x,y);
 	}
 
-	value_type set_pixel(size_t x, size_t y, T v) {
+	value_type set_pixel(int x, int y, T v) {
 		reference r = pixel(x, y); 
 		value_type t = r; r = v; 
 		return t;
 	}
 
-	value_type get_pixel(size_t x, size_t y) const { return pixel(x, y);}
+	value_type get_pixel(int x, int y) const { return pixel(x, y);}
 
-	pointer get_row(size_t y) {
+	pointer get_row(int y) {
 		if(y>=m_height) throw_range_error();
 		uchar_ptr pb = uchar_ptr(m_data) + (m_height - 1 - y)*m_pitch;
 		return pointer(pb);
 	}
 
-	size_t get_width() const {return m_width;}
-	size_t get_height() const {return m_height;}
-	size_t get_depth() const {return m_depth;}
+	int get_width() const {return m_width;}
+	int get_height() const {return m_height;}
+	int get_depth() const {return m_depth;}
 	uchar_ptr get_buffer() {return uchar_ptr(m_data);}
 	
 	// argument is a surface with palette
@@ -137,7 +137,7 @@ class surface {
 		for (int y=m_height-1;y>=0;y--) {
 			pointer ptr = get_row(y);
 			surface<uchar_t>::const_pointer pc = surf.get_row(y);
-			for(size_t x=0;x<m_width;x++) {
+			for(int x=0;x<m_width;x++) {
 				int cix = pc[x];
 				if(cix < n) ptr[x] = pquad[cix];
 				else ptr[x] = 0;
@@ -152,7 +152,7 @@ class surface {
 			pointer ptr = get_row(y);
 			surface<T>::pointer pfrom = from.get_row(y);
 			surface<T>::pointer pto = to.get_row(y);
-			for(size_t x=0;x<m_width;x++) {
+			for(int x=0;x<m_width;x++) {
 				value_type& tfrom = pfrom[x];
 				value_type& tto = pto[x];
 				ptr[x] = value_type(
@@ -169,11 +169,23 @@ class surface {
 			pointer ptr = get_row(y);
 			surface<T>::pointer pfrom = from->get_row(from_dy + y);
 			if(pfrom == 0) break;
-			for(size_t x=0;x<m_width;x++) {
+			for(int x=0;x<m_width;x++) {
 				value_type& tfrom = pfrom[from_dx + x];
 				if(tfrom != transp)
 					ptr[x] = tfrom;
 			}
+		}
+	}
+
+	void rev_rgb_channels() {
+		uchar_ptr pb = uchar_ptr(m_data);
+		for (int y=m_height-1;y>=0;y--) {
+			pointer ptr = pointer(pb);
+			for(int x=0;x<m_width;x++) {
+				uchar_ptr pbt = (uchar_ptr)ptr;
+				*ptr++ = T(*pbt, *(pbt+1), *(pbt+2));
+			}
+			pb += m_pitch;
 		}
 	}
 
@@ -186,10 +198,10 @@ class surface {
 		throw std::range_error("index out of range");
 	}
 
-	size_t m_width;
-	size_t m_height;
-	size_t m_depth;
-	size_t m_pitch;
+	int m_width;
+	int m_height;
+	int m_depth;
+	int m_pitch;
 	pointer m_data;
 };
 
