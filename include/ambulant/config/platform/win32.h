bad682eb3f7b04a9b92ffe9a3d4347f283e4cb29
Kleanthis Kleanthous <kleanthis@sf.net>
2004-04-05 17:15:00 +0000

    Add some WCE and UNICODE-MB related sections

--- include/ambulant/config/platform/win32.h
+++ include/ambulant/config/platform/win32.h
@@ -35,13 +35,86 @@
 
 #endif
 
-
+/////////////////////////////
+// BEGIN WIN32 WINCE SECTION
 #ifdef _WIN32_WCE
+
 #define AMBULANT_PLATFORM_WIN32_WCE
+
+#define AMBULANT_PLATFORM_WIN32_WCE_3
+
+// The following symbol has been used with a different 
+// meaning by other platforms
 #define AMBULANT_NO_IOSTREAMS
+#define AMBULANT_NO_OSTREAM
+#define AMBULANT_NO_STRINGSTREAM
+// Define the following symbol as a replacement for AMBULANT_NO_IOSTREAMS
+// When the following symbol is defined
+// no headers related to streams should be included.
+// Not only the std ones but also ostream.h, istream.h etc
+#define AMBULANT_NO_IOSTREAMS_HEADERS
+
 // implied: #define AMBULANT_NO_STRINGSTREAM
 #define AMBULANT_NO_ABORT
+
+
+#ifdef AMBULANT_PLATFORM_WIN32_WCE_3
+
+// signed/unsigned mismatch
+#pragma warning( disable : 4018)
+
+#include <wce_defs.h>
+typedef unsigned short wchar_t;
+namespace std {
+  using ::ptrdiff_t;
+}
+
+#include <string>
+inline bool operator != (const std::string& s, const char *p) {
+	return !(s == p);
+}
+
+// Actually compiler property
+#define AMBULANT_NO_MEMBER_TEMPLATES
+#define AMBULANT_NO_TIME_H
+
+#elif defined(AMBULANT_PLATFORM_WIN32_WCE_4)
+
+#define abort()
+
 #endif
+
+#endif // _WIN32_WCE
+// END WIN32 WINCE SECTION
+
+/////////////////////////////
+
+///////////////////////////
+// The char type and the string routines are used in many places
+
+#ifdef UNICODE
+// UNICODE
+typedef wchar_t text_char;
+#define text_str(quote) L##quote
+#define text_strchr wcschr
+#define text_strrchr wcsrchr
+#define text_strtok wcstok
+#define text_strlen wcslen
+#define text_vscprintf _vscwprintf
+#define text_strcat wcscat
+#else
+// MB (not UNICODE)
+typedef char text_char;
+#define text_str(quote) quote
+#define text_strchr strchr
+#define text_strrchr strrchr
+#define text_strtok strtok
+#define text_strlen strlen
+#define text_vscprintf _vscprintf
+#define text_strcat strcat
+#endif //UNICODE (#ifdef ... #else ... $endif)
+
+///////////////////////////
 
 //
 // disable min/max macros:
