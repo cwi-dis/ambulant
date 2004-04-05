1b7244dc7696220c26c00e004ad8d11f8f66f661
Kleanthis Kleanthous <kleanthis@sf.net>
2004-04-05 16:52:53 +0000

    Create a src file for area element to simplify debugging. For example when the AM_DBG symbol is defined the area is hifhlighted.

---
+++ src/libambulant/gui/dx/dx_area.cpp
@@ -0,0 +1,108 @@
+/*
+ * 
+ * This file is part of Ambulant Player, www.ambulantplayer.org.
+ * 
+ * Copyright (C) 2003 Stiching CWI, 
+ * Kruislaan 413, 1098 SJ Amsterdam, The Netherlands.
+ * 
+ * Ambulant Player is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * Ambulant Player is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Ambulant Player; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ * In addition, as a special exception, if you link Ambulant Player with
+ * other files to produce an executable, this library does not by itself
+ * cause the resulting executable to be covered by the GNU General Public
+ * License. This exception does not however invalidate any other reason why
+ * the executable file might be covered by the GNU General Public License.
+ * 
+ * As a special exception, the copyright holders of Ambulant Player give
+ * you permission to link Ambulant Player with independent modules that
+ * communicate with Ambulant Player solely through the region and renderer
+ * interfaces, regardless of the license terms of these independent
+ * modules, and to copy and distribute the resulting combined work under
+ * terms of your choice, provided that every copy of the combined work is
+ * accompanied by a complete copy of the source code of Ambulant Player
+ * (the version of Ambulant Player used to produce the combined work),
+ * being distributed under the terms of the GNU General Public License plus
+ * this exception.  An independent module is a module which is not derived
+ * from or based on Ambulant Player.
+ * 
+ * Note that people who make modified versions of Ambulant Player are not
+ * obligated to grant this special exception for their modified versions;
+ * it is their choice whether to do so.  The GNU General Public License
+ * gives permission to release a modified version without this exception;
+ * this exception also makes it possible to release a modified version
+ * which carries forward this exception. 
+ * 
+ */
+
+/* 
+ * @$Id$ 
+ */
+
+#include "ambulant/gui/dx/dx_area.h"
+#include "ambulant/gui/dx/dx_viewport.h"
+#include "ambulant/gui/dx/dx_window.h"
+
+#ifndef AM_DBG
+#define AM_DBG if(0)
+#endif
+
+using namespace ambulant;
+
+gui::dx::dx_area_renderer::dx_area_renderer(
+	common::playable_notification *context,
+	common::playable_notification::cookie_type cookie,
+	const lib::node *node,
+	lib::event_processor* evp,
+	common::abstract_window *window) 
+: common::renderer_playable(context, cookie, node, evp) {
+}
+
+gui::dx::dx_area_renderer::~dx_area_renderer() {
+}
+
+void gui::dx::dx_area_renderer::start(double t) {		
+	if(m_activated) return;	
+	m_dest->show(this);
+	m_dest->need_events(m_wantclicks);
+	m_activated = true;
+}
+
+void gui::dx::dx_area_renderer::stop() {
+	m_dest->renderer_done();
+	m_activated = false;
+}
+
+void gui::dx::dx_area_renderer::redraw(const lib::screen_rect<int> &dirty, 
+	common::abstract_window *window) {
+	AM_DBG {
+		dx_window *dxwindow = static_cast<dx_window*>(window);
+		viewport *v = dxwindow->get_viewport();
+		if(!v) return;
+		
+		lib::screen_rect<int> reg_rc = m_dest->get_rect();
+		lib::point pt = m_dest->get_global_topleft();
+		reg_rc.translate(pt);
+		
+		v->frame_rect(reg_rc);
+	}
+}
+
+void gui::dx::dx_area_renderer::user_event(const lib::point& pt, int what) {
+	if(what == common::user_event_click)
+		m_context->clicked(m_cookie);
+	else if(what == common::user_event_mouse_over) {
+		m_context->pointed(m_cookie);
+	}
+}
