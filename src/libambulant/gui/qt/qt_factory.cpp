f4f6edade668f55f301a5c094602d477500c6a1b
Jack Jansen <Jack.Jansen@cwi.nl>
2004-04-15 11:40:17 +0000

    iAdded missing include.

--- src/libambulant/gui/qt/qt_factory.cpp
+++ src/libambulant/gui/qt/qt_factory.cpp
@@ -51,6 +51,7 @@
 #endif
  
 #include "ambulant/gui/qt/qt_factory.h"
+#include "ambulant/gui/qt/qt_includes.h"
 #include "ambulant/gui/qt/qt_video_renderer.h"
 
 
@@ -72,7 +73,7 @@ qt_video_factory::new_playable(
 	lib::xml_string tag = node->get_qname().second;
     AM_DBG lib::logger::get_logger()->trace("none_video_factory: node 0x%x:   inspecting %s\n", (void *)node, tag.c_str());
 	if ( tag == "video") {
-		rv = new gui::qt::qt_video_renderer(context, cookie, node, evp, m_datasource_factory);
+	  rv = 0; // new gui::qt::qt_video_renderer(context, cookie, node, evp, m_datasource_factory);
 		AM_DBG lib::logger::get_logger()->trace("none_video_factory: node 0x%x: returning none_video_renderer 0x%x", (void *)node, (void *)rv);
 	} else {
 		AM_DBG lib::logger::get_logger()->trace("none_video_factory: no renderer for tag \"%s\"", tag.c_str());
