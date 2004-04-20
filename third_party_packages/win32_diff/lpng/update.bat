ea6a7b1d20f301da8d2f7d67f788ca9e37268543
Kleanthis Kleanthous <kleanthis@sf.net>
2004-04-20 10:38:40 +0000

    Lib PNG and ZLIB distribution update. Download the distributions, decopress and then run the update script to update the distribution.

---
+++ third_party_packages/win32_diff/lpng/update.bat
@@ -0,0 +1,27 @@
+@echo off
+SETLOCAL
+
+
+REM Download libpng from http://sourceforge.net/projects/libpng/
+REM Version 1.2.5
+
+REM Download zlib from http://sourceforge.net/projects/libpng/
+REM Version 1.2.1
+
+REM Decompress both under the same directory
+REM For example:
+REM third_party_packages
+REM 	lpng125
+REM 	zlib
+
+REM Run this script to update the distribution
+
+REM The directory where the libpng lib was decompressed
+set LPNG_HOME=..\..\lpng125
+
+REM Copy project files
+copy libpng.* %LPNG_HOME%\projects\msvc
+copy zlib.* %LPNG_HOME%\projects\msvc
+
+ENDLOCAL
+@echo on
