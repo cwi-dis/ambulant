0f2153804b0aecc649f58ef9ad9e272b5d1638fc
Kleanthis Kleanthous <kleanthis@sf.net>
2004-04-20 08:43:42 +0000

    Start an attempt to automate the creation of the third party packages in the form required by win32/wince from the official distributions. Mainly copying the required projects but also other changes when required.

---
+++ third_party_packages/win32_diff/jpeg/update.bat
@@ -0,0 +1,29 @@
+@echo off
+SETLOCAL
+
+REM Download the JPEG lib from http://www.ijg.org/files/
+REM Ambulant has been tested with jpeg-6b
+REM Decompress the distribution and run this script to update the distribution
+
+REM The directory where the jpeg lib was decompressed
+set JPEG_HOME=..\..\jpeg-6b
+
+REM The current directory
+set UPD_HOME=%~dp0
+
+REM Create the project directories
+if not exist %JPEG_HOME%\win32 md %JPEG_HOME%\win32
+if not exist %JPEG_HOME%\wince md %JPEG_HOME%\wince
+
+REM Ambulant needs to update jinclude.h
+cd %JPEG_HOME%
+rename jinclude.h jinclude.h.org
+cd %UPD_HOME%
+copy %UPD_HOME%\jinclude.h %JPEG_HOME%\jinclude.h
+
+REM Copy project files
+copy %UPD_HOME%\win32\*.* %JPEG_HOME%\win32
+copy %UPD_HOME%\wince\*.* %JPEG_HOME%\wince
+
+ENDLOCAL
+@echo on
