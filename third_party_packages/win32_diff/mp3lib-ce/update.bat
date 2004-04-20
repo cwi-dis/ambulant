9bd0912727f8d6afd29a938e0af4506221bc4663
Kleanthis Kleanthous <kleanthis@sf.net>
2004-04-20 12:26:42 +0000

    For completeness document the mp3lib used following the same format. Though this lib is not directly available.

---
+++ third_party_packages/win32_diff/mp3lib-ce/update.bat
@@ -0,0 +1,20 @@
+@echo off
+REM $Id$
+SETLOCAL
+
+REM MP3LIB 
+
+REM WinCE only
+
+REM Custom version of a lib obtained from 
+REM http://www.projectmayo.com/ 
+REM (cvs.projectmayo.com)
+REM some years ago
+REM Future versions of Ambulant may use a different MP3 lib
+
+REM The custom version used currently can be downloaded 
+REM from http://www.ambulant.org
+
+
+ENDLOCAL
+@echo on
