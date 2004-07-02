f182f941dcb31f5d6542a0ba2b9892cbf9b47104
Jack Jansen <Jack.Jansen@cwi.nl>
2004-07-02 12:35:02 +0000

    Updated version numbers, etc.

--- src/player_wince/resource.h
+++ src/player_wince/resource.h
@@ -21,7 +21,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32777
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
