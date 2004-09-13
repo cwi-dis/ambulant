b63b06327428350f69752ce6005307fa55b1e40b
Daniel Benden <koelimoe@sf.net>
2004-09-13 14:23:35 +0000

    structure needed to remove SDL from the qt_renderer

---
+++ include/ambulant/common/factory.h
@@ -0,0 +1,16 @@
+#ifndef _FACTORY_H
+#define _FACTORY_H
+
+#include "ambulant/net/datasource.h"
+#include "ambulant/common/playable.h"
+
+struct factories {
+	datasource_factory *m_df;
+	playable_factory   *m_rf;
+}
+	
+
+
+	
+
+#endif /* _FACTORY_H */
