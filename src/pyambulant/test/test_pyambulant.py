b6420ed08a40b8c651be512ae168e9d806a8a104
Jack Jansen <Jack.Jansen@cwi.nl>
2005-07-01 21:02:16 +0000

    Hit the first snag: constructors aren't supported.

--- src/pyambulant/test/test_pyambulant.py
+++ src/pyambulant/test/test_pyambulant.py
@@ -1,6 +1,7 @@
 # Test suite for pyambulant.
 import ambulant
 import unittest
+import time
 
 VERSION="1.5" # Ambulant version
 
@@ -9,9 +10,39 @@ class TestBasics(unittest.TestCase):
     def setUp(self):
         pass
         
-    def test1getversion(self):
+    def test01getversion(self):
         version = ambulant.get_version()
         self.assertEqual(version, VERSION)
+        
+    def test02realtimetimer(self):
+        rtt = ambulant.realtime_timer_factory()
+        self.assert_(type(rtt) is ambulant.abstract_timer)
+        t1 = rtt.elapsed()
+        self.assert_(t1 > 0)
+        time.sleep(1)
+        t2 = rtt.elapsed()
+        self.assertAlmostEqual(t1+1000, t2, -1)
+        
+    def test03eventprocessor(self):
+        rtt = ambulant.realtime_timer_factory()
+        evp = ambulant.event_processor_factory(rtt)
+        self.assert_(type(evp) is ambulant.event_processor)
+        time.sleep(1)
+        t1 = rtt.elapsed()
+        evp_timer = evp.get_timer_1()
+        t2 = evp_timer.elapsed()
+        self.assertAlmostEqual(t1, t2, -1)
+        
+    def test04document(self):
+        rf = ambulant.playable_factory()
+        wf = ambulant.window_factory()
+        df = ambulant.datasource_factory()
+        pf = ambulant.get_parser_factory()
+        factories = (rf, wf, df, pf)
+        doc = ambulant.create_from_string(factories, "<smil></smil>", "file:///test.smil")
+        self.assert_(type(doc) is ambulant.document)
+        root = doc.get_root()
+        self.assert_(type(root) is ambulant.node)
         
 if __name__ == "__main__":
     unittest.main()
