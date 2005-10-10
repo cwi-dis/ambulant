6bc0d3b5f6d884474b88e387c62bcd207d4f6ff5
Jack Jansen <Jack.Jansen@cwi.nl>
2005-10-10 12:53:14 +0000

    Fixed build procedure on 10.4 (Help file indexing).

--- src/player_macosx/genappleindex.sh
+++ src/player_macosx/genappleindex.sh
@@ -7,17 +7,4 @@ x)
 	helpdir=$1
 	;;
 esac
-osascript << XYZZY
-tell application "Finder"
-    activate
-    set machelpdir to POSIX file "$helpdir"
-    set folderAlias to machelpdir as alias 
-end tell
-
-tell application "Apple Help Indexing Tool"
-    activate 
-    use tokenizer "English" 
-    turn anchor indexing "on" 
-    open folderAlias 
-end tell
-XYZZY
+"/Developer/Applications/Utilities/Help Indexer.app/Contents/MacOS/Help Indexer" $helpdir -IndexAnchors -TigerIndexing
