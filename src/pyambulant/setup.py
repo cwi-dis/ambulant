293004297e197ae64f0b941e21f898809d87af2a
Jack Jansen <Jack.Jansen@cwi.nl>
2005-11-01 12:41:43 +0000

    More tweaks to make pyambulant build on Linux.

--- src/pyambulant/setup.py
+++ src/pyambulant/setup.py
@@ -1,18 +1,33 @@
 from distutils.core import setup, Extension
+import sys
+import os
+
 DEFS =  [
     ('USE_SMIL21', '1'),
     ('WITH_EXTERNAL_DOM', '1'),
     ('ENABLE_NLS', '1'),
     ]
+    
+EXTRA_LINK_ARGS=[]
+ldflags=os.getenv("LDFLAGS")
+if ldflags:
+    EXTRA_LINK_ARGS=ldflags.split()
+if sys.platform == 'darwin':
+    EXTRA_LINK_ARGS += ['-framework', 'CoreFoundation']
 
+LIBRARIES=['ambulant', 'expat']
+LIBDIRS=['../../third_party_packages/expat-unix/lib']
+if sys.platform == 'darwin':
+    LIBRARIES += ['intl']
+    
 setup(name='ambulant',
       version='0.1',
       ext_modules=[
         Extension('ambulant',
             ['ambulantmodule.cpp', 'ambulantinterface.cpp', 'ambulantutilities.cpp'],
-            libraries=['ambulant', 'expat', 'intl'],
-            extra_link_args=['-framework', 'CoreFoundation'],
-            library_dirs=['../../third_party_packages/expat-unix/lib'],
+            libraries=LIBRARIES,
+            extra_link_args=EXTRA_LINK_ARGS,
+            library_dirs=LIBDIRS,
             include_dirs=['../../include'],
             define_macros=DEFS
         )
