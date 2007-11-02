eb56fd01b58c41f04db4b419b482ad569f98e0b9
Jack Jansen <Jack.Jansen@cwi.nl>
2007-11-02 13:38:05 +0000

    Also copy SDL and live555 libraries.

--- projects/vc8-wince5-smil3/copy_ffmpeg_libs.bat
+++ projects/vc8-wince5-smil3/copy_ffmpeg_libs.bat
@@ -13,3 +13,21 @@ rem Copy and convert .def files to .lib files
 lib /machine:arm /out:..\..\lib\wince-arm\avformat.lib /def:%FFMPEG%\libavformat\avformat-51.def
 lib /machine:arm /out:..\..\lib\wince-arm\avcodec.lib /def:%FFMPEG%\libavcodec\avcodec-51.def
 lib /machine:arm /out:..\..\lib\wince-arm\avutil.lib /def:%FFMPEG%\libavutil\avutil-49.def
+rem
+rem Copy SDL library
+set SDL="..\..\third_party_packages\SDL-1.2.12-wm5\VisualCE\SDL\Windows Mobile 5.0 Pocket PC SDK (ARMV4I)"
+rem XXX copy %SDL%\Release\SDL.lib ..\..\lib\wince-arm\SDL.lib
+rem XXX copy %SDL%\Release\SDL.dll ..\..\bin\wince\SDL.dll
+copy %SDL%\Debug\SDL.lib ..\..\lib\wince-arm\SDL.lib
+copy %SDL%\Debug\SDL.dll ..\..\bin\wince\SDL.dll
+rem
+rem Copy live555 libraries
+set LIVE="..\..\third_party_packages\live_VC8_wince\BUILD"
+copy %LIVE%\BasicUsageEnvironment-Debug\BasicUsageEnvironmentD.lib ..\..\lib\wince-arm\BasicUsageEnvironmentD.lib
+copy %LIVE%\UsageEnvironment-Debug\UsageEnvironmentD.lib ..\..\lib\wince-arm\UsageEnvironmentD.lib
+copy %LIVE%\groupsock-Debug\groupsockD.lib ..\..\lib\wince-arm\groupsockD.lib
+copy %LIVE%\liveMedia-Debug\liveMediaD.lib ..\..\lib\wince-arm\liveMediaD.lib
+rem XXX copy %LIVE%\BasicUsageEnvironment-Release\BasicUsageEnvironment.lib ..\..\lib\wince-arm\BasicUsageEnvironment.lib
+rem XXX copy %LIVE%\UsageEnvironment-Release\UsageEnvironment.lib ..\..\lib\wince-arm\UsageEnvironment.lib
+rem XXX copy %LIVE%\groupsock-Release\groupsock.lib ..\..\lib\wince-arm\groupsock.lib
+rem XXX copy %LIVE%\liveMedia-Release\liveMedia.lib ..\..\lib\wince-arm\liveMedia.lib
