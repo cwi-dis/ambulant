b1207e06b69db8ea75879f31866ef918f73a5e20
Jack Jansen <Jack.Jansen@cwi.nl>
2008-01-14 15:21:13 +0000

    Added missing break. This makes textConceal=final work.

--- src/libambulant/gui/cocoa/cocoa_smiltext.mm
+++ src/libambulant/gui/cocoa/cocoa_smiltext.mm
@@ -430,13 +430,22 @@ cocoa_smiltext_renderer::redraw_body(const rect &dirty, gui_window *window)
 		double now = m_event_processor->get_timer()->elapsed() - m_epoch;
 		logical_origin.x += now * m_params.m_rate / 1000;
 		visible_origin.x -= now * m_params.m_rate / 1000;
+		// XXX see below
 	}
 	if (m_params.m_mode == smil2::stm_scroll) {
 		double now = m_event_processor->get_timer()->elapsed() - m_epoch;
+#if 0
 		logical_origin.y += now * m_params.m_rate / 1000;
 		visible_origin.y -= now * m_params.m_rate / 1000;
+#else
+		visible_origin.y -= now * m_params.m_rate / 1000;
+		if (visible_origin.y < 0) {
+			logical_origin.y -= visible_origin.y;
+			// visible_origin.y = 0;
+		}
+#endif
 	}
-	AM_DBG logger::get_logger()->debug("cocoa_smiltext_renderer.redraw at Cocoa-point (%f, %f)", visible_origin.x, visible_origin.y);
+	/*AM_DBG*/ logger::get_logger()->debug("cocoa_smiltext_renderer.redraw at Cocoa-point (%f, %f) logical (%f, %f)", visible_origin.x, visible_origin.y, logical_origin.x, logical_origin.y);
 	if (m_render_offscreen) {
 	}
 #if 0
@@ -447,7 +456,7 @@ cocoa_smiltext_renderer::redraw_body(const rect &dirty, gui_window *window)
 	// for ticker tape, so we adjust.
 	NSRect logical_rect = NSMakeRect(logical_origin.x, logical_origin.y, visible_size.width, visible_size.height);
 	NSRange glyph_range = [m_layout_manager glyphRangeForBoundingRect: logical_rect inTextContainer: m_text_container];
-	AM_DBG NSLog(@"Glyph range was %d, %d, origin-x %f", glyph_range.location, glyph_range.length, logical_origin.x);
+	/*AM_DBG*/ NSLog(@"Glyph range was %d, %d, origin-x %f", glyph_range.location, glyph_range.length, logical_origin.x);
 #if 0
 	float fraction;
 	unsigned leftpoint = [ m_layout_manager glyphIndexForPoint: logical_origin inTextContainer: m_text_container 
@@ -461,7 +470,7 @@ cocoa_smiltext_renderer::redraw_body(const rect &dirty, gui_window *window)
 		NSRect used_rect = [m_layout_manager boundingRectForGlyphRange: glyph_range inTextContainer: m_text_container];
 		visible_origin.x -= used_rect.origin.x;
 	}
-	AM_DBG NSLog(@"Glyph range is now %d, %d", glyph_range.location, glyph_range.length);
+	/*AM_DBG*/ NSLog(@"Glyph range is now %d, %d", glyph_range.location, glyph_range.length);
 #endif
 #endif
 	if (glyph_range.location >= 0 && glyph_range.length > 0) {
@@ -550,6 +559,7 @@ cocoa_smiltext_renderer::_compute_rate(lib::size size, lib::rect r,  unsigned in
 				dst = win/2+txt;
 				break;
 			}
+			break;
 		case smil2::stc_both: // ignore textPlace
 			dst = win+txt;
 			break;
