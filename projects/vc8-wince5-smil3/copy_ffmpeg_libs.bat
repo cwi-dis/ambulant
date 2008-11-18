893ecf7841f1ef63ab4609515c1af6c570fad7c6
Jack Jansen <Jack.Jansen@cwi.nl>
2008-11-18 16:02:48 +0000

    Integrate copying/converting of ffmpeg libs into tpp build.

--- projects/vc8-wince5-smil3/copy_ffmpeg_libs.bat
+++ projects/vc8-wince5-smil3/copy_ffmpeg_libs.bat
@@ -5,12 +5,12 @@ call "%VS80COMNTOOLS%vsvars32.bat"
 rem
 rem Copy DLLs
 set FFMPEG="..\..\third_party_packages\ffmpeg-wm5"
-copy %FFMPEG%\libavformat\avformat-51.dll ..\..\bin\wince\avformat-51.dll
+copy %FFMPEG%\libavformat\avformat-52.dll ..\..\bin\wince\avformat-52.dll
 copy %FFMPEG%\libavcodec\avcodec-51.dll ..\..\bin\wince\avcodec-51.dll
 copy %FFMPEG%\libavutil\avutil-49.dll ..\..\bin\wince\avutil-49.dll
 rem
 rem Copy and convert .def files to .lib files
-lib /machine:arm /out:..\..\lib\wince-arm\avformat.lib /def:%FFMPEG%\libavformat\avformat-51.def
+lib /machine:arm /out:..\..\lib\wince-arm\avformat.lib /def:%FFMPEG%\libavformat\avformat-52.def
 lib /machine:arm /out:..\..\lib\wince-arm\avcodec.lib /def:%FFMPEG%\libavcodec\avcodec-51.def
 lib /machine:arm /out:..\..\lib\wince-arm\avutil.lib /def:%FFMPEG%\libavutil\avutil-49.def
 rem
