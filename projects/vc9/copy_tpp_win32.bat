a14d58e742c496bb874dd14ec01a5db029095036
Jack Jansen <Jack.Jansen@cwi.nl>
2009-05-20 12:03:11 +0000

    Getting started on putting all pathnames for third party packages in a single place: Ambulant-win32.vsprops.
    Not completely done yet for third party packages.

--- projects/vc9/copy_tpp_win32.bat
+++ projects/vc9/copy_tpp_win32.bat
@@ -14,37 +14,36 @@ if not exist %AMB_HOME%\bin md %AMB_HOME%\bin
 if not exist %AMB_HOME%\bin\win32 md %AMB_HOME%\bin\win32
 
 REM Expat
-copy %TPP_HOME%\expat\lib\Release\libexpat.lib %AMB_HOME%\lib\win32\libexpat.lib
+copy %expat_dir%\lib\Release\libexpat.lib %AMB_HOME%\lib\win32\libexpat.lib
 
 REM PNG DLL
-copy %TPP_HOME%\lpng128\projects\visualc71\Win32_LIB_ASM_Release\libpng.lib %AMB_HOME%\lib\win32\libpng.lib
+copy %lpng_dir%\projects\visualc71\Win32_LIB_ASM_Release\libpng.lib %AMB_HOME%\lib\win32\libpng.lib
 
 REM ZLib DLL
-copy %TPP_HOME%\lpng128\projects\visualc71\Win32_LIB_Release\zlib\zlib.lib %AMB_HOME%\lib\win32\zlib.lib
+copy %lpng_dir%projects\visualc71\Win32_LIB_Release\zlib\zlib.lib %AMB_HOME%\lib\win32\zlib.lib
 
 REM JPEG static library
-copy %TPP_HOME%\jpeg\win32\Release\libjpeg.lib %AMB_HOME%\lib\win32\libjpeg.lib
+copy %jpeg_dir%\win32\Release\libjpeg.lib %AMB_HOME%\lib\win32\libjpeg.lib
 
 REM Xerces lib, if it exists.
-set XER_BUILD=%TPP_HOME%\xerces-c-src_2_8_0\Build\Win32\VC8\Release
-set XERD_BUILD=%TPP_HOME%\xerces-c-src_2_8_0\Build\Win32\VC8\Debug
+set XER_BUILD=%xerces_dir%\Build\Win32\VC8\Release
 if exist %XER_BUILD% copy %XER_BUILD%\xerces-c_2.lib %AMB_HOME%\lib\win32\xerces-c_2.lib
 if exist %XER_BUILD% copy %XER_BUILD%\xerces-c_2_8.dll %AMB_HOME%\bin\win32\xerces-c_2_8.dll
 if exist %XER_BUILD% copy %XERD_BUILD%\xerces-c_2D.lib %AMB_HOME%\lib\win32\xerces-c_2D.lib
 if exist %XER_BUILD% copy %XERD_BUILD%\xerces-c_2_8D.dll %AMB_HOME%\bin\win32\xerces-c_2_8D.dll
 
 REM ffmpeg
-copy %TPP_HOME%\ffmpeg\libavcodec\avcodec-51.dll %AMB_HOME%\bin\win32\avcodec-51.dll
-copy %TPP_HOME%\ffmpeg\libavcodec\avcodec-51.lib %AMB_HOME%\lib\win32\avcodec.lib
-copy %TPP_HOME%\ffmpeg\libavformat\avformat-52.dll %AMB_HOME%\bin\win32\avformat-52.dll
-copy %TPP_HOME%\ffmpeg\libavformat\avformat-52.lib %AMB_HOME%\lib\win32\avformat.lib
-copy %TPP_HOME%\ffmpeg\libavutil\avutil-49.dll %AMB_HOME%\bin\win32\avutil-49.dll
-copy %TPP_HOME%\ffmpeg\libavutil\avutil-49.lib %AMB_HOME%\lib\win32\avutil.lib
+copy %ffmpeg_dir%\libavcodec\avcodec-51.dll %AMB_HOME%\bin\win32\avcodec-51.dll
+copy %ffmpeg_dir%\libavcodec\avcodec-51.lib %AMB_HOME%\lib\win32\avcodec.lib
+copy %ffmpeg_dir%\libavformat\avformat-52.dll %AMB_HOME%\bin\win32\avformat-52.dll
+copy %ffmpeg_dir%\libavformat\avformat-52.lib %AMB_HOME%\lib\win32\avformat.lib
+copy %ffmpeg_dir%\libavutil\avutil-49.dll %AMB_HOME%\bin\win32\avutil-49.dll
+copy %ffmpeg_dir%\libavutil\avutil-49.lib %AMB_HOME%\lib\win32\avutil.lib
 
 REM sdl
-copy %TPP_HOME%\SDL-1.2.12\lib\SDL.dll %AMB_HOME%\bin\win32\SDL.dll
-copy %TPP_HOME%\SDL-1.2.12\lib\SDL.lib %AMB_HOME%\lib\win32\SDL.lib
-copy %TPP_HOME%\SDL-1.2.12\lib\SDLmain.lib %AMB_HOME%\lib\win32\SDLmain.lib
+copy %sdl_dir%\lib\SDL.dll %AMB_HOME%\bin\win32\SDL.dll
+copy %sdl_dir%\lib\SDL.lib %AMB_HOME%\lib\win32\SDL.lib
+copy %sdl_dir%\lib\SDLmain.lib %AMB_HOME%\lib\win32\SDLmain.lib
 
 REM Live555
 copy %TPP_HOME%\live_VC9\BUILD\BasicUsageEnvironment-Release\BasicUsageEnvironment.lib %AMB_HOME%\lib\win32\BasicUsageEnvironment.lib
@@ -53,5 +52,5 @@ copy %TPP_HOME%\live_VC9\BUILD\liveMedia-Release\liveMedia.lib %AMB_HOME%\lib\wi
 copy %TPP_HOME%\live_VC9\BUILD\UsageEnvironment-Release\UsageEnvironment.lib %AMB_HOME%\lib\win32\UsageEnvironment.lib
 
 REM libxml2
-copy %TPP_HOME%\libxml2-2.7.3\win32\bin.msvc\libxml2_a.lib %AMB_HOME%\lib\win32\libxml2_a.lib
+copy %libxml2_dir%\win32\bin.msvc\libxml2_a.lib %AMB_HOME%\lib\win32\libxml2_a.lib
 @echo on
