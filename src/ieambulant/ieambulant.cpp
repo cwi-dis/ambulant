40ae65607c2a2c42837116a1458480941037772f
Kees Blom <Kees.Blom@cwi.nl>
2009-06-03 16:10:31 +0000

    1st version of Ambulant as an Internet Explorer plugin. ieambulantButtons.htm has working buttons both with Explorer/Firefox, Safari/Opera untested but should work. ieambulant2.htm attempts to start 2 simultaneous instances, but does not work.

---
+++ src/ieambulant/ieambulant.cpp
@@ -0,0 +1,366 @@
+// ieambulant.cpp : Implementation of Cieambulant
+
+#include "stdafx.h"
+#include "comutil.h"
+#include "AmbulantActiveX.h"
+#include "ieambulant.h"
+#include <fstream>
+#include <string>
+
+/////////////////////////////////////////////////////////////////////////////
+// Cieambulant
+
+// convert BSTR to LPSTR, allocates new char[] 
+LPSTR
+ConvertBSTRToLPSTR (BSTR bstrIn)
+{
+  	LPSTR pszOut = NULL;
+   
+   	if (bstrIn != NULL)
+   	{
+   		int nInputStrLen = SysStringLen (bstrIn);
+   		// Double NULL Termination
+   		int nOutputStrLen = WideCharToMultiByte(CP_ACP, 0, bstrIn, nInputStrLen, NULL, 0, 0, 0) + 2;	
+   
+   		pszOut = new char [nOutputStrLen];
+   		if (pszOut)
+   		{
+   		    memset (pszOut, 0x00, sizeof (char)*nOutputStrLen);
+ 			WideCharToMultiByte (CP_ACP, 0, bstrIn, nInputStrLen, pszOut, nOutputStrLen, 0, 0);
+   		}
+   	}
+	return pszOut;
+}
+
+HRESULT
+Cieambulant::get_document_url() 
+{
+	HRESULT hr = E_FAIL;
+	if ( ! m_site)
+		return hr;
+	IServiceProvider* pISP = NULL;
+	hr = m_site->QueryInterface(IID_IServiceProvider, (void **)&pISP);
+	if ( ! SUCCEEDED(hr))
+		return hr;
+	IWebBrowser2* pIWebBrowser2 = NULL;
+	hr = pISP->QueryService(IID_IWebBrowserApp, IID_IWebBrowser2,
+					   (void **)&pIWebBrowser2);
+	if ( ! SUCCEEDED(hr))
+		return hr;
+	BSTR BSTR_base_url;
+	hr = pIWebBrowser2->get_LocationURL(&BSTR_base_url);
+	if ( ! SUCCEEDED(hr))
+		return hr;
+	LPSTR LPSTR_url = ConvertBSTRToLPSTR (BSTR_base_url);
+	SysFreeString(BSTR_base_url);
+	BSTR_base_url = NULL;
+	std::string std_string_base_url (LPSTR_url);
+	m_base_url = ambulant::net::url::from_url(std_string_base_url);
+	delete [] LPSTR_url;
+	IDispatch *dispatch = NULL;
+	LPPROPERTYBAG pPropBag = (IPropertyBag*) malloc (sizeof IPropertyBag);
+	hr = this->IPersistPropertyBag_Load(pPropBag, NULL, NULL);
+	hr = pIWebBrowser2->get_Document(&dispatch);
+	if ( ! SUCCEEDED(hr) || dispatch == NULL)
+		return hr;
+	IHTMLDocument3* document3;
+	hr = dispatch->QueryInterface(IID_IHTMLDocument3,
+		                          (void **)&document3);
+	dispatch->Release();
+	if ( ! SUCCEEDED(hr) || document3 == NULL)
+		return hr;
+	BSTR BSTR_object = SysAllocString(L"object");
+	IHTMLElementCollection* pelColl;
+	hr = document3->getElementsByTagName(BSTR_object, &pelColl);
+	document3->Release();
+	SysFreeString(BSTR_object);
+	if ( ! SUCCEEDED(hr))
+		return hr;
+	VARIANT itemIndex;
+	itemIndex.vt = VT_I4;
+	itemIndex.lVal = 0;
+	IDispatch *pElemDisp;
+	hr = pelColl->item(itemIndex, itemIndex, &pElemDisp);
+	pelColl->Release();
+	if ( ! SUCCEEDED(hr))
+		return hr;
+	IHTMLElement* pHTMLElement;
+	hr = pElemDisp->QueryInterface(IID_IHTMLElement, (void**) &pHTMLElement);
+	pElemDisp->Release();
+	if ( ! SUCCEEDED(hr))
+		return hr;
+	BSTR BSTR_src = SysAllocString(L"src");
+	VARIANT var;
+	hr = pHTMLElement->getAttribute(BSTR_src, 0, &var);
+	pHTMLElement->Release();
+	if ( ! SUCCEEDED(hr))
+		return hr;
+	if (var.vt != VT_NULL) {
+		LPSTR_url = ConvertBSTRToLPSTR (_bstr_t(var));	
+		std::string std_string_url (LPSTR_url);
+		m_url = ambulant::net::url::from_url(std_string_url);
+		delete [] LPSTR_url;
+	}
+}
+/* MSG spy
+static HHOOK s_hook;
+LRESULT  CALLBACK
+GetMsgProc(int   nCode,WPARAM   wparam,LPARAM   lparam)   
+{  
+    MSG * msg = (MSG *)lparam;
+	ambulant::lib::logger::get_logger()->debug("MSG=0x%x",msg->message);
+	if( msg->message == WM_COMMAND) {
+		//analyse the command message 
+		ambulant::lib::logger::get_logger()->debug("nCode=0x%x, wparam=0x%x",nCode,wparam);
+    }
+    return   CallNextHookEx(s_hook,nCode,wparam,lparam);   
+}
+*/
+
+STDMETHODIMP 
+Cieambulant::SetClientSite(LPOLECLIENTSITE pSite)
+{
+    HRESULT hr = CComControlBase::IOleObject_SetClientSite(pSite);
+	if (hr != S_OK)
+		return hr;
+	m_site = pSite;
+//    if(pSite && !m_pFont)
+//		hr = GetAmbientFontDisp(&m_pFont);
+    GetAmbientBackColor(m_clrBackColor);
+    GetAmbientForeColor(m_clrForeColor);
+	//XXX std::ostream sos = new std::ostream(std::cout);
+	//XXX ambulant::lib::logger::get_logger()->set_std_ostream(sos);
+	static std::ofstream log_os("C:\\Documents and Settings\\kees.AMBULANT-DEV\\My Documents\\Ambulant\\ambulant\\src\\ieambulant\\amlog.txt");
+	ambulant::lib::logger::get_logger()->set_std_ostream(log_os);
+//	s_hook = SetWindowsHookEx(WH_GETMESSAGE, GetMsgProc, NULL, GetCurrentThreadId());
+	
+	return hr;
+}
+
+void
+DrawString( HDC hdc, RECT* rc, BSTR caption )
+	{
+		USES_CONVERSION;		
+		TCHAR* pCaption = OLE2T(caption);
+
+		DrawText( hdc,
+				 pCaption,
+				 lstrlen( pCaption ),
+				 rc,
+				 DT_WORDBREAK );
+	}
+
+// STDMETHODIMP HRESULT 
+HRESULT
+Cieambulant::OnDraw(ATL_DRAWINFO& di)
+{
+	return S_OK;
+}
+HRESULT
+Cieambulant::updatePlayerState()
+{
+	// added for ambulant
+	// here the <url> is obtained from the <PARAM name="src" value="<url>"/> element
+	// and joined with  the base url (see get_document_location())
+	if (m_url.get_url() == "")
+		get_document_url();
+	if ( ! m_url.is_absolute()) {
+		ambulant::net::url tmp_url;    
+        if (m_base_url.get_url() != "")
+            m_url = m_url.join_to_base(m_base_url);
+    }
+	if (m_hwnd == NULL) {
+		m_hwnd = ::GetWindow(this->m_hWnd, GW_HWNDFIRST);
+		m_player_callbacks.set_os_window(m_hwnd);
+	}
+	if (m_ambulant_player == NULL) {
+		m_player_callbacks.set_os_window(m_hwnd);
+		m_ambulant_player = new ambulant::gui::dx::dx_player(m_player_callbacks, NULL, m_url);
+//X		m_ambulant_player->set_state_component_factory(NULL); // XXXJACK DEBUG!!!!
+		if (m_ambulant_player) {
+			if ( ! get_player()) {
+				delete m_ambulant_player;
+				m_ambulant_player = NULL;
+			} 
+		}
+	}
+	if (m_ambulant_player != NULL) {
+		if (m_bstrPlayerState == "playing"
+			&& ! m_ambulant_player->get_player()->is_playing())
+			m_ambulant_player->play();
+		else if (m_bstrPlayerState == "pausing"
+			&& ! m_ambulant_player->get_player()->is_pausing())
+			m_ambulant_player->pause();
+		else if (m_bstrPlayerState == "stopped"
+			&& (m_ambulant_player->get_player()->is_playing()
+				|| m_ambulant_player->get_player()->is_pausing()))
+			m_ambulant_player->stop();
+		else if (m_bstrPlayerState == "restarting") {
+			m_ambulant_player->stop();
+			m_ambulant_player->play();
+			m_bstrPlayerState = "playing";
+		}
+	}
+	return S_OK;
+}
+// some platform/toolkit specific hacks and functions
+
+static ambulant_player_callbacks s_ambulant_player_callbacks;
+
+LRESULT CALLBACK
+Cieambulant::PluginWinProc(UINT msg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+{
+	typedef HRGN NPRegion;
+	if (updatePlayerState() != S_OK)
+		return S_FALSE;
+
+	switch (msg) {
+		case WM_PAINT:
+			{
+				PAINTSTRUCT ps;
+				HDC hdc = BeginPaint(&ps);
+				RECT rc;
+				GetClientRect(&rc);
+				FrameRect(hdc, &rc, GetStockBrush(BLACK_BRUSH));
+				EndPaint(&ps);
+				if (m_hwnd && m_ambulant_player) {
+					m_ambulant_player->redraw(m_hwnd, hdc);
+					::ShowWindow(m_hwnd, SW_SHOW);
+				}
+				break;
+			}
+			break;
+		case WM_LBUTTONDOWN:
+		case WM_MOUSEMOVE:
+			{
+				POINT point;
+				point.x=GET_X_LPARAM(lParam);
+				point.y=GET_Y_LPARAM(lParam);
+
+				if (m_ambulant_player) {
+					if (msg == WM_MOUSEMOVE) {
+						// code copied from MmView.cpp
+						int new_cursor_id = m_ambulant_player->get_cursor(point.x, point.y, m_hwnd);
+//XX					if (new_cursor_id>0) EnableToolTips(TRUE);
+//XX					else CancelToolTips();
+						if(new_cursor_id != m_cursor_id) {
+							HINSTANCE hIns = 0;
+							HCURSOR new_cursor = 0;
+							if(new_cursor_id == 0) {
+								new_cursor = LoadCursor(hIns, IDC_ARROW); 
+							} else {
+								new_cursor = LoadCursor(hIns, IDC_HAND); 
+							}
+//XX						SetClassLongPtr(GCLP_HCURSOR, HandleToLong(new_cursor));
+							m_cursor_id = new_cursor_id;
+						}
+					} else {
+						m_ambulant_player->on_click(point.x, point.y, m_hwnd);
+                    }
+                }
+                break;
+                
+            default:
+                break;
+            }
+        }
+	BOOL rv = DefWindowProc(msg, wParam, lParam);
+	bHandled = rv;
+	return rv;
+}
+
+ambulant_player_callbacks::ambulant_player_callbacks()
+:	m_hwnd(NULL)
+{
+}
+
+void
+ambulant_player_callbacks::set_os_window(HWND hwnd)
+{
+	m_hwnd = hwnd;
+}
+
+
+HWND 
+ambulant_player_callbacks::new_os_window()
+{
+	return m_hwnd;
+}
+
+SIZE
+ambulant_player_callbacks::get_default_size()
+{
+	SIZE size;
+	size.cx = ambulant::common::default_layout_width;
+	size.cy = ambulant::common::default_layout_height;
+	return size;
+}
+
+void
+ambulant_player_callbacks::destroy_os_window(HWND hwnd)
+{
+	m_hwnd = NULL;
+}
+
+html_browser*
+ambulant_player_callbacks::new_html_browser(int left, int top, int width, int height)
+{
+	return NULL; // not implemented, but needs to be declared
+}
+
+STDMETHODIMP
+Cieambulant::get_mimeType(BSTR *pVal)
+{
+	if (!pVal)
+    {
+        return E_INVALIDARG;
+    }
+    *pVal = m_bstrType.Copy();
+	return S_OK;
+}
+
+STDMETHODIMP
+Cieambulant::put_mimeType(BSTR newVal)
+{
+    m_bstrType.Empty();
+    m_bstrType.Attach(SysAllocString(newVal));
+	return S_OK;
+}
+
+STDMETHODIMP
+Cieambulant::get_src(BSTR *pVal)
+{
+	if (!pVal)
+    {
+        return E_INVALIDARG;
+    }
+    *pVal = m_bstrSrc.Copy();
+	return S_OK;
+}
+
+STDMETHODIMP
+Cieambulant::put_src(BSTR newVal)
+{
+    m_bstrSrc.Empty();
+    m_bstrSrc.Attach(SysAllocString(newVal));
+	return S_OK;
+}
+
+STDMETHODIMP
+Cieambulant::get_playerState(BSTR *pVal)
+{
+	if (!pVal)
+    {
+        return E_INVALIDARG;
+    }
+    *pVal = m_bstrPlayerState.Copy();
+	return S_OK;
+}
+
+STDMETHODIMP
+Cieambulant::put_playerState(BSTR newVal)
+{
+    m_bstrPlayerState.Empty();
+    m_bstrPlayerState.Attach(SysAllocString(newVal));
+	return S_OK;
+}
---
+++ src/ieambulant/ieambulant.h
@@ -0,0 +1,270 @@
+// ieambulant.h : Declaration of the Cieambulant
+
+#ifndef __ieambulant_H_
+#define __ieambulant_H_
+
+#include "resource.h"       // main symbols
+#include <atlctl.h>
+
+// ambulant player includes
+#include "ambulant/version.h"
+#include "ambulant/common/player.h"
+#include "ambulant/common/plugin_engine.h"
+#include "ambulant/net/url.h"
+#include "ambulant/lib/logger.h"
+#include <windowsx.h>
+
+//XXX begin copied from npambulant.h
+#include <ambulant/gui/dx/dx_player.h>
+#include <ambulant/net/url.h>
+#include "AmbulantActiveX.h"
+#include "_IieambulantEvents_CP.H"
+typedef ambulant::gui::dx::dx_player_callbacks gui_callbacks; //XX from MmView.cpp
+
+class ambulant_player_callbacks : public ambulant::gui::dx::dx_player_callbacks {
+
+ public:
+	ambulant_player_callbacks();
+	void set_os_window(HWND hwnd);
+	HWND new_os_window();
+	SIZE get_default_size();
+	void destroy_os_window(HWND);
+	html_browser *new_html_browser(int left, int top, int width, int height);
+    HWND m_hwnd;
+};
+//XXX end copied from npambulant.h
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Cieambulant
+class ATL_NO_VTABLE Cieambulant : 
+	public CComObjectRootEx<CComSingleThreadModel>,
+	public CStockPropImpl<Cieambulant, Iieambulant, &IID_Iieambulant, &LIBID_AmbulantACTIVEXLib>,
+	public CComControl<Cieambulant>,
+	public IPersistStreamInitImpl<Cieambulant>,
+	public IPersistPropertyBagImpl<Cieambulant>,
+	public IObjectSafetyImpl<Cieambulant, 
+                        INTERFACESAFE_FOR_UNTRUSTED_CALLER | 
+                        INTERFACESAFE_FOR_UNTRUSTED_DATA>,
+	public IOleControlImpl<Cieambulant>,
+	public IOleObjectImpl<Cieambulant>,
+	public IOleInPlaceActiveObjectImpl<Cieambulant>,
+	public IViewObjectExImpl<Cieambulant>,
+	public IOleInPlaceObjectWindowlessImpl<Cieambulant>,
+	public ISupportErrorInfo,
+	public IConnectionPointContainerImpl<Cieambulant>,
+	public IPersistStorageImpl<Cieambulant>,
+	public ISpecifyPropertyPagesImpl<Cieambulant>,
+	public IQuickActivateImpl<Cieambulant>,
+	public IDataObjectImpl<Cieambulant>,
+	public IProvideClassInfo2Impl<&CLSID_ieambulant, &DIID__IieambulantEvents, &LIBID_AmbulantACTIVEXLib>,
+	public IPropertyNotifySinkCP<Cieambulant>,
+	public CComCoClass<Cieambulant, &CLSID_ieambulant>
+{
+public:
+	CContainedWindow m_ctlStatic;
+	
+
+	Cieambulant() :
+		m_ctlStatic(_T("Static"), this, 1)
+	{
+		m_OldWindow = NULL;
+		m_hwnd = NULL;
+		m_lpOldProc = NULL;
+		m_logger = NULL;
+		m_ambulant_player = NULL;
+		m_bWindowOnly = TRUE;
+/* JNK
+		m_bstrCaption = "OK";
+		CComPtr<IOleClientSite>  pIOleClientSite;
+		CComPtr<IOleInPlaceSite> pIOleInPlaceSite;
+		HRESULT hRes = GetClientSite(&pIOleClientSite);
+		if (pIOleClientSite) {
+			if(SUCCEEDED(pIOleClientSite->QueryInterface(IID_IOleInPlaceSite, (void **)&pIOleInPlaceSite)) && pIOleInPlaceSite) {
+				pIOleInPlaceSite->GetWindow(&m_hwnd);       
+			}
+		}
+		*/
+		m_bstrCaption = "playing";
+		m_bstrPlayerState = m_bstrCaption;
+		//m_url = ambulant::net::url::from_filename("file://C:\\Documents and Settings\\kees.AMBULANT-DEV\\My Documents\\Ambulant\\ambulant\\Extras\\Welcome\\Welcome.smil");
+	}
+	~Cieambulant() {
+		if (m_ambulant_player) {
+			m_ambulant_player->stop();
+			delete m_ambulant_player;
+			m_ambulant_player = NULL;
+		}
+	}
+DECLARE_REGISTRY_RESOURCEID(IDR_ieambulant)
+
+DECLARE_PROTECT_FINAL_CONSTRUCT()
+
+BEGIN_COM_MAP(Cieambulant)
+	COM_INTERFACE_ENTRY(Iieambulant)
+	COM_INTERFACE_ENTRY(IDispatch)
+	COM_INTERFACE_ENTRY(IViewObjectEx)
+	COM_INTERFACE_ENTRY(IViewObject2)
+	COM_INTERFACE_ENTRY(IViewObject)
+	COM_INTERFACE_ENTRY(IOleInPlaceObjectWindowless)
+	COM_INTERFACE_ENTRY(IOleInPlaceObject)
+	COM_INTERFACE_ENTRY2(IOleWindow, IOleInPlaceObjectWindowless)
+	COM_INTERFACE_ENTRY(IOleInPlaceActiveObject)
+	COM_INTERFACE_ENTRY(IOleControl)
+	COM_INTERFACE_ENTRY(IOleObject)
+	COM_INTERFACE_ENTRY(IPersistStreamInit)
+	COM_INTERFACE_ENTRY(IPersistPropertyBag)
+	COM_INTERFACE_ENTRY(IObjectSafety)
+	COM_INTERFACE_ENTRY2(IPersist, IPersistStreamInit)
+	COM_INTERFACE_ENTRY(ISupportErrorInfo)
+	COM_INTERFACE_ENTRY(IConnectionPointContainer)
+	COM_INTERFACE_ENTRY(ISpecifyPropertyPages)
+	COM_INTERFACE_ENTRY(IQuickActivate)
+	COM_INTERFACE_ENTRY(IPersistStorage)
+	COM_INTERFACE_ENTRY(IDataObject)
+	COM_INTERFACE_ENTRY(IProvideClassInfo)
+	COM_INTERFACE_ENTRY(IProvideClassInfo2)
+END_COM_MAP()
+
+BEGIN_PROP_MAP(Cieambulant)
+	PROP_DATA_ENTRY("_cx", m_sizeExtent.cx, VT_UI4)
+	PROP_DATA_ENTRY("_cy", m_sizeExtent.cy, VT_UI4)
+	PROP_ENTRY("BackColor", DISPID_BACKCOLOR, CLSID_StockColorPage)
+	PROP_ENTRY("Caption", DISPID_CAPTION, CLSID_NULL)
+	PROP_ENTRY("Font", DISPID_FONT, CLSID_StockFontPage)
+	PROP_ENTRY("ForeColor", DISPID_FORECOLOR, CLSID_StockColorPage)
+    // AmbulantPlayer plugin control properties
+    PROP_ENTRY("type", 1, CLSID_NULL)
+    PROP_ENTRY("src", 2, CLSID_NULL)
+    PROP_ENTRY("playerState", 3, CLSID_NULL)
+ 	// Example entries
+	// PROP_ENTRY("Property Description", dispid, clsid)
+	// PROP_PAGE(CLSID_StockColorPage)
+END_PROP_MAP()
+
+BEGIN_CONNECTION_POINT_MAP(Cieambulant)
+	CONNECTION_POINT_ENTRY(IID_IPropertyNotifySink)
+END_CONNECTION_POINT_MAP()
+
+BEGIN_MSG_MAP(Cieambulant)
+	MESSAGE_HANDLER(WM_CREATE, OnCreate)
+	MESSAGE_HANDLER(WM_SETFOCUS, OnSetFocus)
+	MESSAGE_HANDLER(WM_PAINT, PluginWinProc)
+	CHAIN_MSG_MAP(CComControl<Cieambulant>)
+ALT_MSG_MAP(1)
+	// Replace this with message map entries for superclassed Static
+END_MSG_MAP()
+// Handler prototypes:
+//  LRESULT MessageHandler(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
+//  LRESULT CommandHandler(WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled);
+//  LRESULT NotifyHandler(int idCtrl, LPNMHDR pnmh, BOOL& bHandled);
+
+// ISupportsErrorInfo
+	STDMETHOD(InterfaceSupportsErrorInfo)(REFIID riid)
+	{
+		static const IID* arr[] = 
+		{
+			&IID_Iieambulant,
+		};
+		for (int i=0; i<sizeof(arr)/sizeof(arr[0]); i++)
+		{
+			if (InlineIsEqualGUID(*arr[i], riid))
+				return S_OK;
+		}
+		return S_FALSE;
+	}
+
+
+	LRESULT OnSetFocus(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+	{
+		LRESULT lRes = CComControl<Cieambulant>::OnSetFocus(uMsg, wParam, lParam, bHandled);
+		if (m_bInPlaceActive)
+		{
+			DoVerbUIActivate(&m_rcPos,  NULL);
+			if(!IsChild(::GetFocus()))
+				m_ctlStatic.SetFocus();
+		}
+		return lRes;
+	}
+
+	LRESULT OnCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+	{
+		RECT rc;
+		GetWindowRect(&rc);
+		rc.right -= rc.left;
+		rc.bottom -= rc.top;
+		rc.top = rc.left = 0;
+		//m_ctlStatic.Create(m_hWnd, rc);
+		m_ctlStatic.Create( m_hWnd,
+                    rc, 
+                    _T("Static"),
+                    WS_CHILD|WM_PAINT);
+		return 0;
+	}
+	LRESULT CALLBACK PluginWinProc(UINT, WPARAM, LPARAM, BOOL& );
+	STDMETHOD(SetObjectRects)(LPCRECT prcPos,LPCRECT prcClip)
+	{
+		IOleInPlaceObjectWindowlessImpl<Cieambulant>::SetObjectRects(prcPos, prcClip);
+		int cx, cy;
+		cx = prcPos->right - prcPos->left;
+		cy = prcPos->bottom - prcPos->top;
+		::SetWindowPos(m_ctlStatic.m_hWnd, NULL, 0,
+			0, cx, cy, SWP_NOZORDER | SWP_NOACTIVATE /*XXX | SWP_SHOWWINDOW */);
+		return S_OK;
+	}
+
+	STDMETHOD(SetClientSite)(LPOLECLIENTSITE pSite);
+	HRESULT OnDraw(ATL_DRAWINFO& di);
+// IViewObjectEx
+	DECLARE_VIEW_STATUS(VIEWSTATUS_SOLIDBKGND | VIEWSTATUS_OPAQUE)
+
+public:
+	OLE_COLOR m_clrBackColor;
+	CComBSTR m_bstrCaption;
+//	CComPtr<IFontDisp> m_pFont;
+	OLE_COLOR m_clrForeColor;
+
+// added for Ambulant player
+public:
+	CComBSTR m_bstrType;
+	CComBSTR m_bstrSrc;
+
+	CComBSTR m_bstrPlayerState;
+	HRESULT updatePlayerState();
+
+	ambulant_player_callbacks m_player_callbacks;
+	LPOLECLIENTSITE m_site;
+	HRESULT get_document_url();
+	HWND m_hwnd;
+	WNDPROC m_lpOldProc;
+	LONG m_OldWindow;
+    ambulant::gui::dx::dx_player* m_ambulant_player;
+	ambulant::lib::logger* m_logger;
+	ambulant::net::url m_url;
+	ambulant::net::url m_base_url;
+	int m_cursor_id;
+    ambulant::common::player* get_player() {
+        return m_ambulant_player->get_player();
+    }
+/*
+from http://www.ozhiker.com/electronics/vcpp/ActiveXControl.htm
+To make ATL think your CWnd derived class is a CWindow derived class, add the following method:
+*/
+static LPCTSTR GetWndClassName()
+{
+    return NULL;
+}
+// added for Ambulant
+//XX IPersistPropertyBag override
+//XX	STDMETHOD(Load)(LPPROPERTYBAG pPropBag, LPERRORLOG pErrorLog);
+public:
+	STDMETHOD(get_src)(/*[out, retval]*/ BSTR *pVal);
+	STDMETHOD(put_src)(/*[in]*/ BSTR newVal);
+	STDMETHOD(get_mimeType)(/*[out, retval]*/ BSTR *pVal);
+	STDMETHOD(put_mimeType)(/*[in]*/ BSTR newVal);
+	STDMETHOD(get_playerState)(/*[out, retval]*/ BSTR *pVal);
+	STDMETHOD(put_playerState)(/*[in]*/ BSTR newVal);
+
+};
+
+#endif //__ieambulant_H_
