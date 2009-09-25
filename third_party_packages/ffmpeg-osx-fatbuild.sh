62ea2911240885fff13d8d543aa1bf63a0ca6c02
Jack Jansen <Jack.Jansen@cwi.nl>
2009-09-25 22:47:29 +0000

    Got the script to work with ffmpeg 0.5.
    Also added code to build for any selection of the 4 architectures
    (ppc, i386, ppc64, x86_64) but this is currently untested.

--- third_party_packages/ffmpeg-osx-fatbuild.sh
+++ third_party_packages/ffmpeg-osx-fatbuild.sh
@@ -1,15 +1,48 @@
 #!/bin/sh
 #
-#CONFIGOPTS="--enable-swscale --disable-mmx --disable-encoders --enable-gpl --disable-vhook --disable-ffmpeg"
-#CONFIGOPTS="--disable-optimizations --disable-stripping --disable-mmx --enable-gpl --disable-vhook --disable-ffserver --disable-ffmpeg --enable-static --enable-shared --enable-libfaad --disable-libfaac --extra-cflags=-I/usr/local/include --disable-altivec"
-CONFIGOPTS="--disable-mmx --disable-encoders --enable-gpl --disable-vhook --disable-ffserver --disable-ffmpeg --enable-static --enable-shared --enable-libfaad --disable-libfaac --extra-cflags=-I/usr/local/include --disable-altivec"
-srcdir=$1
-# XXXX Enable these for doing 10.4-compatible distributions
+# Build universal MacOSX ffmpeg libraries.
+#
+# Script created by Jack.Jansen@cwi.nl, as part of the Ambulant project.
+# This version of the script is public domain, so feel free to add any
+# license you want if you modify it (but open would be preferred, of course!)
+#
+# Last tested with ffmpeg 0.5.
+#
+
+#
+# Set this to the global options you want to configure ffmpeg with.
+#
+CONFIGOPTS="--disable-encoders --enable-swscale --enable-gpl --disable-vhook --disable-ffserver --disable-ffmpeg --enable-static --enable-shared --enable-libfaad --disable-libfaac --extra-cflags=-I/usr/local/include"
+#
+# If you want to build for a different MacOSX version than the current one
+# define SYSROOT and MACOSX_DEPLOYMENT_TARGET
+#
 SYSROOT=" -isysroot /Developer/SDKs/MacOSX10.4u.sdk"
 export MACOSX_DEPLOYMENT_TARGET=10.4
-# XXXX Enable these for native 10.5 distributions
-#SYSROOT=""
-#export MACOSX_DEPLOYMENT_TARGET=10.5
+#
+# Set variables here to true to include the ABI
+#
+PPC=true
+I386=true
+PPC64=false
+X86_64=false
+#
+# Set this variable to any of the architectures you are build
+# (used to pick up .pc files)
+#
+ANY_RANDOM_ARCH=i386
+#
+# You may need to fiddle these, but probably not.
+#
+PPC_CONFIGOPTS="--arch=powerpc --cpu=g4 --enable-altivec --enable-shared"
+I386_CONFIGOPTS="--arch=i686 --cpu=i686 --enable-shared"
+PPC64_CONFIGOPTS="--enable-cross-compile --arch=ppc64 --cpu=g4 --enable-altivec --enable-shared"
+X86_64_CONFIGOPTS="--arch=x86_64 --cpu=i686 --enable-shared"
+
+#
+# Should be boilerplate from here on
+#
+srcdir=$1
 case x$srcdir in
     x)
         echo Usage: $0 ffmpeg-src-dir '[all|mkdirs|mklinks|configure|clean|build|merge|install] ...'
@@ -66,8 +99,10 @@ done
 
 if $mkdirs; then
     echo $0: mkdirs
-    mkdir build-i386
-    mkdir build-ppc
+    if $I386; then mkdir build-i386; fi
+    if $PPC; then mkdir build-ppc; fi
+    if $X86_64; then mkdir build-x86_64; fi
+    if $PPC64; then mkdir build-ppc64; fi
     mkdir libavformat
     mkdir libavcodec
     mkdir libavutil
@@ -87,64 +122,121 @@ else
 fi
 
 if $configure; then
-    echo $0: configure i386
-    (
-        cd build-i386
-        $srcdir/configure \
-            --extra-cflags="-arch i386 $SYSROOT" \
-            --extra-ldflags="-arch i386 $SYSROOT" \
-            --arch=i686 \
-            --cpu=i686 \
-            --enable-shared \
-            $CONFIGOPTS
-    )
-    echo $0: configure ppc
-    (
-        cd build-ppc
-        $srcdir/configure \
-            --extra-cflags="-arch ppc $SYSROOT" \
-            --extra-ldflags="-arch ppc $SYSROOT" \
-            --arch=powerpc \
-            --cpu=g4 \
-            --enable-altivec \
-            --enable-shared \
-            $CONFIGOPTS
-    )
+	if $I386; then
+		echo $0: configure i386
+		(
+			cd build-i386
+			$srcdir/configure \
+				--extra-cflags="-arch i386 $SYSROOT" \
+				--extra-ldflags="-arch i386 $SYSROOT" \
+				$I386_CONFIGOPTS \
+				$CONFIGOPTS
+		)
+	else
+		echo $0: skipping configure for i386
+	fi
+	if $PPC; then
+		echo $0: configure ppc
+		(
+			cd build-ppc
+			$srcdir/configure \
+				--extra-cflags="-arch ppc $SYSROOT" \
+				--extra-ldflags="-arch ppc $SYSROOT" \
+				$PPC_CONFIGOPTS \
+				$CONFIGOPTS
+		)
+	else
+		echo $0: skipping configure for ppc
+	fi
+	if $PPC64; then
+		echo $0: configure ppc64
+		(
+			cd build-ppc64
+			$srcdir/configure \
+				--extra-cflags="-arch ppc64 $SYSROOT" \
+				--extra-ldflags="-arch ppc64 $SYSROOT" \
+				$PPC64_CONFIGOPTS \
+				$CONFIGOPTS
+		)
+	else
+		echo $0: skipping configure for ppc64
+	fi
+	if $X86_64; then
+		echo $0: configure x86_64
+		(
+			cd build-x86_64
+			$srcdir/configure \
+				--extra-cflags="-arch x86_64 $SYSROOT" \
+				--extra-ldflags="-arch x86_64 $SYSROOT" \
+				$X86_64_CONFIGOPTS \
+				$CONFIGOPTS
+		)
+	else
+		echo $0: skipping configure for x86_64
+	fi
 else
     echo $0: skipping configure
 fi
 
 if $clean; then
-    echo $0: clean i386
-    ( cd build-i386; make clean)
-    echo $0: clean ppc
-    ( cd build-ppc; make clean)
+	if $I386; then
+		echo $0: clean i386
+		( cd build-i386; make clean)
+	fi
+	if $PPC; then
+		echo $0: clean ppc
+		( cd build-ppc; make clean)
+	fi
+	if $PPC64; then
+		echo $0: clean ppc64
+		( cd build-ppc64; make clean)
+	fi
+	if $X86_64; then
+		echo $0: clean x86_64
+		( cd build-x86_64; make clean)
+	fi
 else
     echo $0: skipping clean
 fi
 
 if $build; then
-    echo $0: build i386
-    ( cd build-i386; make)
-    echo $0: build ppc
-    ( cd build-ppc; make)
+	if $I386; then
+		echo $0: build i386
+		( cd build-i386; make)
+	else
+		echo $0: skipping build for i386
+	fi
+	if $PPC; then
+		echo $0: build ppc
+		( cd build-ppc; make)
+	else
+		echo $0: skipping build for ppc
+	fi
+	if $PPC64; then
+		echo $0: build ppc64
+		( cd build-ppc64; make)
+	else
+		echo $0: skipping build for ppc64
+	fi
+	if $X86_64; then
+		echo $0: build x86_64
+		( cd build-x86_64; make)
+	else
+		echo $0: skipping build for x86_64
+	fi
 else
     echo $0: skipping build
 fi
 
 if $merge; then
     echo $0: merge
-    lipo -create -output libavformat/libavformat.a build-{i386,ppc}/libavformat/libavformat.a
-    lipo -create -output libavcodec/libavcodec.a build-{i386,ppc}/libavcodec/libavcodec.a
-    lipo -create -output libavutil/libavutil.a build-{i386,ppc}/libavutil/libavutil.a
-    #lipo -create -output libswscale/libswscale.a build-{i386,ppc}/libswscale/libswscale.a
-    # XXXX Should also do the dynamic libraries?
-    # Also need to copy .pc files. Do it here.
-    # NOTE: this is correct as of ffmpeg 15-jun-08, expected to change soon (ffmpeg are
-    # moving their .pc files around).
-    cp build-i386/*.pc .    
-    # Here's the newer(?) version:
-    #cp build-i386/libav{format,util,codec}/*.pc .
+    lipo -create -output libavformat/libavformat.a build-*/libavformat/libavformat.a
+    lipo -create -output libavcodec/libavcodec.a build-*/libavcodec/libavcodec.a
+    lipo -create -output libavutil/libavutil.a build-*/libavutil/libavutil.a
+    lipo -create -output libswscale/libswscale.a build-*/libswscale/libswscale.a
+    for i in format util codec swscale; do
+    	cp build-$ANY_RANDOM_ARCH/libav$i/*.pc libav$i/
+    done
 else
     echo $0: skipping merge
 fi
