887cb11aa76e93275b66e5c8b170a6774ea2281f
Jack Jansen <Jack.Jansen@cwi.nl>
2010-03-24 21:25:54 +0000

    After some serious hacking, creating the installer DMG now no longer requires manual intervention.

--- installers/sh-macos/mkmacdist.sh
+++ installers/sh-macos/mkmacdist.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 scriptdir=`dirname $0`
 topdir=$scriptdir/../..
+interactive=false
 dirname=$1
+installroot=$2
 if [ x$dirname == x ]; then
-	echo Usage: $0 dirname
+	echo Usage: $0 dirname [installroot]
 	echo dirname is where the distribution will be created.
 	echo Something like Ambulant-1.8-mac is what we usually use.
 	exit 2
@@ -16,21 +18,46 @@ if [ -f $dirname.dmg ]; then
 	echo $dirname.dmg already exists
 	exit 1
 fi
+#
+# Create the directory
+#
 mkdir $dirname
-
+#
+# Unpack the template (which contains icon position, etc)
+#
+(cd $dirname ; tar xf ../foldertemplate.tar)
+#
+# Copy all other files
+#
 cp $topdir/README $dirname/README
-cp -r "/Applications/Ambulant Player.app" "$dirname/Ambulant Player.app"
+rm -rf "$dirname/Ambulant Player.app"
+cp -r "$installroot/Applications/Ambulant Player.app" "$dirname/Ambulant Player.app"
 ln -s /Applications $dirname/Applications
 cp -r $topdir/Extras/DemoPresentation $dirname/DemoPresentation
 find $dirname/DemoPresentation -name 'CVS' -a -exec rm -r '{}' ';'
 cp $topdir/COPYING $dirname/COPYING
-mkdir $dirname/.folderbg
-cp $scriptdir/folderbg.png $dirname/.folderbg/folderbg.png
+#mkdir $dirname/.folderbg
+#cp $scriptdir/folderbg.png $dirname/.folderbg/folderbg.png
 
 hdiutil create -srcfolder $dirname $dirname-rw.dmg -format UDRW -attach
-echo Please give the folder the layout you want, select bgimage from \".folderbg\", unmount, press return.
-read a
-
+if $interactive; then
+	echo Please give the folder the layout you want, select bgimage from \".folderbg\", unmount, press return.
+	read a
+	
+else
+	osascript - << END-OF-SCRIPT
+tell application "Finder"
+	open disk "$dirname"
+	set the background picture of icon view options of Finder window 1 to document file "folderbg.png" of item ".folderbg" of disk "$dirname"
+	get the background picture of icon view options of Finder window 1
+	set the background picture of icon view options of Finder window 1 to document file "folderbg.png" of item ".folderbg" of disk "$dirname"
+	get the background picture of icon view options of Finder window 1
+end tell
+END-OF-SCRIPT
+	sleep 2
+	hdiutil detach /Volumes/$dirname
+	sleep 2
+fi
 hdiutil convert $dirname-rw.dmg -format UDZO -o $dirname.dmg
 rm $dirname-rw.dmg
 rm -r $dirname
