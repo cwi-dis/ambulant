1bc7551e4a2d147955165018522672dd004429d8
Kees Blom <Kees.Blom@cwi.nl>
2011-02-18 13:13:59 +0100

    Test notification mail

--- src/libambulant/gui/cg/cg_transition.mm
+++ src/libambulant/gui/cg/cg_transition.mm
@@ -404,8 +404,7 @@ cg_transition_blitclass_polylist::update()
 	CGContextDrawLayerInRect(ctx, cg_fullsrcrect, [view getTransitionSurface]);
 	CGContextRestoreGState(ctx);
 }
-
-	
+
 smil2::transition_engine *
 cg_transition_engine(common::surface *dst, bool is_outtrans, const lib::transition_info *info)
 {
