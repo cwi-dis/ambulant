1f9e976d82ea5651a7401e301298e670f49daf9c
Jack Jansen <Jack.Jansen@cwi.nl>
2011-02-23 17:28:08 +0100

    Fixed webkit plugin installer builder.

--- installers/mkwebkitplugindist.sh
+++ installers/mkwebkitplugindist.sh
@@ -8,12 +8,12 @@ if [ ! -f src/webkit_plugin/README ]; then
 	echo Please run only in toplevel AMbulant source directory
 	exit
 fi
-if [ ! -d ~/Library/Internet\ Plug-Ins/AmbulantWebKitPlugin.plugin ; then
-	echo No plugin in ~/Library/Internet\ Plug-Ins/AmbulantWebKitPlugin.plugin
+if [ ! -d ~/Library/Internet\ Plug-Ins/AmbulantWebKitPlugin.webplugin ]; then
+	echo No plugin in ~/Library/Internet\ Plug-Ins/AmbulantWebKitPlugin.webplugin
 	exit
 fi
 rm -rf $1
 mkdir $1
-cp -R ~/Library/Internet\ Plug-Ins/AmbulantWebKitPlugin.plugin $1
+cp -R ~/Library/Internet\ Plug-Ins/AmbulantWebKitPlugin.webplugin $1
 cp README $1
 zip -r $1.zip $1
