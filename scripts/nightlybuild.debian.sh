73c34dc6d27c9b3812c14b70c2c4ba6406fd330e
Jack Jansen <Jack.Jansen@cwi.nl>
2011-03-14 17:43:45 +0100

    Adding index files

--- scripts/nightlybuild.debian.sh
+++ scripts/nightlybuild.debian.sh
@@ -61,7 +61,10 @@ cd ..
 cd ..
 mkdir debian-$TODAY
 mv *.tar.gz *.deb *.dsc *.changes *.build debian-$TODAY/
+dpkg-scanpackages debian-$TODAY | gzip -9c > Packages.gz
+dpkg-scansources debian-$TODAT | gzip -9c > Sources.gz
 scp -r debian-$TODAY $DESTINATION
+scp Packages.gz Sources.gz $DESTINATION
 
 #
 # Delete old installers, remember current
