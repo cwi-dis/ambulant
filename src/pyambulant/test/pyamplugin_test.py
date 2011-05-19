24550e8233be55b5718b64b8b10c7b7b2e6703a7
Kees Blom <Kees.Blom@cwi.nl>
2011-05-19 12:53:48 +0200

    Added 'today()'

--- src/pyambulant/test/pyamplugin_test.py
+++ src/pyambulant/test/pyamplugin_test.py
@@ -9,10 +9,11 @@ import sys
 sys.stderr = sys.stdout = open('AM_TEST-output.txt', 'w')
 import ambulant
 import time
+import datetime
 
 # First check: the ambulant glue works.
 
-print 'TEST', 0, 'test_framework_imported', ambulant.get_version()
+print 'TEST', 0, 'test_framework_imported', ambulant.get_version(), 'on: am' datetime.date.today()
 
 class Reporting_feedback(ambulant.player_feedback):
     def __init__(self):
