b1abb185d76338fd87d5d5cd31ec31ba31610cd6
Jack Jansen <Jack.Jansen@cwi.nl>
2011-05-25 11:17:54 +0200

    Added correct copyright preamble to iPhone files

--- src/player_iphone/AmbulantWebViewController.mm
+++ src/player_iphone/AmbulantWebViewController.mm
@@ -1,10 +1,21 @@
-    //
-//  AmbulantWebViewController.mm
-//  player_iphone
+// This file is part of Ambulant Player, www.ambulantplayer.org.
 //
-//  Created by Kees Blom on 8/7/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
+// Copyright (C) 2003-2010 Stichting CWI,
+// Science Park 123, 1098 XG Amsterdam, The Netherlands.
 //
+// Ambulant Player is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// Ambulant Player is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Ambulant Player; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #import "AmbulantWebViewController.h"
 
