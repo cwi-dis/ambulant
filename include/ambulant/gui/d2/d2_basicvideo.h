a1f159cd5da218b89cd0ddaf70581648d97a92c2
Jack Jansen <Jack.Jansen@cwi.nl>
2011-05-27 14:52:33 +0200

    WITH_DX_EVR bites the dust.

--- include/ambulant/gui/d2/d2_basicvideo.h
+++ include/ambulant/gui/d2/d2_basicvideo.h
@@ -19,10 +19,6 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-/*
- * @$Id$
- */
-
 #ifndef AMBULANT_GUI_D2_BASICVIDEO_H
 #define AMBULANT_GUI_D2_BASICVIDEO_H
 
@@ -32,8 +28,6 @@
 #include "ambulant/gui/d2/d2_player.h"
 #include "ambulant/lib/mtsync.h"
 
-#define WITH_DX_EVR
-
 interface IGraphBuilder;
 interface IMediaControl;
 interface IMediaPosition;
@@ -41,9 +35,7 @@ interface IMediaEvent;
 interface IBasicAudio;
 interface IVideoWindow;
 interface IBaseFilter;
-#ifdef WITH_DX_EVR
 interface IMFVideoDisplayControl;
-#endif
 
 namespace ambulant {
 
@@ -97,11 +89,9 @@ class d2_basicvideo_renderer : public common::renderer_playable {
 	IMediaEvent *m_media_event;
 	IBasicAudio *m_basic_audio;
 	IVideoWindow *m_video_window;
-#ifdef WITH_DX_EVR
 	IBaseFilter *m_evr;
 	IMFVideoDisplayControl *m_evr_control;
 	HWND m_evr_hwnd;
-#endif
 
 	d2_player *m_d2player;
 	lib::critical_section m_cs;
