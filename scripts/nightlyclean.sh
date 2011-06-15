42e192bad19010c09e3369849849d8789de29803
Jack Jansen <Jack.Jansen@cwi.nl>
2011-06-15 10:37:44 +0200

    Fixes

--- scripts/nightlyclean.sh
+++ scripts/nightlyclean.sh
@@ -1,13 +1,14 @@
+#!/bin/sh
 # Clean out old nightly builds on the ambulantplayer.org website.
 BUILDDIR=/var/www/AmbulantPlayerOrg/nightlybuilds
 set -x
 set -x
 cd $BUILDDIR
-subdirs=`echo */*/ | grep -v docs`
+subdirs=`echo */*/`
 for dir in $subdirs; do
 	cd $BUILDDIR
 	cd $dir
-	ls -t | grep '20[0-9][0-9][0-9][0-9][0-9][0-9]' | tail -n +6 | xargs echo 'RM '
+	ls -t | grep '20[0-9][0-9][0-9][0-9][0-9][0-9]' | tail -n +6 | xargs rm
 done
 
 	
