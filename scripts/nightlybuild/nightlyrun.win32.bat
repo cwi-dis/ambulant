fe6dbd54f0b3f60cdee9ab801ca658a78c7b5031
Jack Jansen <Jack.Jansen@cwi.nl>
2011-09-06 16:14:48 +0200

    Notes on how to run

--- scripts/nightlybuild/nightlyrun.win32.bat
+++ scripts/nightlybuild/nightlyrun.win32.bat
@@ -2,6 +2,12 @@ rem
 rem  Script to do a nightly clean build of a full Ambulant, indirectly
 rem  Windows version
 rem 
+rem Run this script automatically by having the user auto-login, and add this
+rem script to to the "Task Scheduler" for the "logon" event (with elevated permissions).
+rem See http://www.techrepublic.com/blog/window-on-windows/make-vista-launch-uac-restricted-programs-at-startup-with-task-scheduler/616
+rem
+rem It will not work correctly as gpedit.msc login script, for some reason.
+
 set python="c:\python26\python.exe"
 
 cd \users\jack\documents
