d50cdb5e534c5373cd70020382e40b4a1527ade2
Jack Jansen <Jack.Jansen@cwi.nl>
2012-01-05 12:22:55 +0100

    Changes in hostname

--- scripts/nightlybuild/nightlyrun.debian.sh
+++ scripts/nightlybuild/nightlyrun.debian.sh
@@ -5,5 +5,5 @@
 set -e
 set -x 
 curl http://ambulantplayer.org/cgi-bin/hgweb.cgi/hg/ambulant/raw-file/default/scripts/nightlybuild/nightlybuild.debian.sh | sh
-ssh moes prlctl stop Ubuntu1104 > /dev/null 2>&1 &
+ssh moes prlctl stop Ubuntu-1104-64bit > /dev/null 2>&1 &
 
