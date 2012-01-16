b1101e8f3e81fde06d6523ad24cc6db26e662c59
Kees Blom <kees@cwi.nl>
2012-01-16 15:19:37 +0100

    Changed --without-x into --with-x=no after reading SDL's ./configure file

--- scripts/build-third-party-packages.py
+++ scripts/build-third-party-packages.py
@@ -405,7 +405,7 @@ third_party_packages={
             checkcmd="pkg-config --atleast-version=1.3.0 sdl",
             buildcmd=
                 "cd SDL-1.3.0-* && "
-                "./configure --prefix='%s' --without-x --disable-dependency-tracking "
+                "./configure --prefix='%s'  --enable-video-x11=no  --enable-video-opengl=no --with-x=no --disable-dependency-tracking "
                     "CFLAGS='%s -framework ForceFeedback' "
                     "LDFLAGS='%s -framework ForceFeedback' &&"
                 "make ${MAKEFLAGS} && "
@@ -683,7 +683,7 @@ third_party_packages={
             checkcmd="test -f %s/lib/libSDL.a" % COMMON_INSTALLDIR,
             buildcmd=
                 "cd SDL-1.3.0-*  && "
-                "./configure --enable-video-x11=no  --enable-video-opengl=no &&"                
+                "./configure --with-x=no --enable-video-x11=no  --enable-video-opengl=no &&"                
                 "cd Xcode-iOS/SDL  && "
                 "xcodebuild -target libSDL -sdk iphonesimulator%s -configuration Debug &&"
                 "mkdir -p ../../../installed/include/SDL && cp ../../include/* ../../../installed/include/SDL &&"
