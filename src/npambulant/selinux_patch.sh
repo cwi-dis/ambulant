72cdeac10f3b7bd95d88b6d3e67815e5b49009f2
Jack Jansen <Jack.Jansen@cwi.nl>
2012-01-23 14:28:22 +0100

    Getting rid of dead wood

--- src/npambulant/selinux_patch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# with seliunxenabled, set appropriate security context
-PATH=${PATH}:/sbin:/usr/sbin ;
-which chcon selinuxenabled 2>1 1>/dev/null && selinuxenabled;
-if [ $? == 0 -a $1 ];then chcon -t texrel_shlib_t $1; fi
