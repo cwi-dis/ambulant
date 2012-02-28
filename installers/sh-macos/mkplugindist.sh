3a1fb67ad0881519553acd37f764af50bdc45543
Kees Blom <kees@cwi.nl>
2012-02-28 12:57:37 +0100

    The installer for npambulant.plugin is now constructed from a template .dmg file, otherwise folder background may not appear.

--- installers/sh-macos/mkplugindist.sh
+++ installers/sh-macos/mkplugindist.sh
@@ -33,61 +33,44 @@ if [ ! -d "$plugin_srcdir" ]; then
 	exit 3
 fi
 #
-# Create the directory
+# This installer is constructed from a template 'npambulant-template.dmg', which was created
+# using 'Disk Utility', were its Volume name (npambulant-plugin) was defined, and
+# the background image was defined by creating and copying an initial background image
+# in /Volumes/npambulant-plugin/.background/background.png, then in 'Terminal' entering
+# the commands 'open  /Volumes/npambulant-plugin/.background' and using the 'Finder'
+# window 'npambulant-plugin' select 'View->Show View Option'. Then, in the 'View Options' window
+# under 'Background' select 'Image' and drag file 'backgound.png' in the '.background' window
+# into the provided space. Then the disk 'npambulant-plugin' was 'Eject'ed and its corresponding .dmg
+# was compressed using: 'hdiutil convert npambulant-plugin-rw.dmg -format UDZO -o npambulant-plugin.dmg'
+# The template has all visuals (icons and background), but the files are empty.
+# Their contents are copied in by executing this script.
+# This seems to be more reliable than fiddling with 'osascript', because some
+# 'Finder' versions seem ot use "optimized" techniques to handle folder backgrounds. Based on:
+# http://stackoverflow.com/questions/96882/how-do-i-create-a-nice-looking-dmg-for-mac-os-x-using-command-line-tools, 8th answer
 #
-mkdir $dirname
+# Create the new .dmg by copying the template
 #
-# Unpack the template (which contains files needed and their icon position, etc)
+cp npambulant-template.dmg $dirname.dmg 
 #
-cat $scriptdir/plugintemplate.tar | (cd $dirname ; tar xf -; mv placeholder.plugin $plugin_name)
+# Convert it into a writable image and attach it to the file system (mount)
+#
+hdiutil convert $dirname.dmg -format UDRW -o $dirname-rw.dmg
+hdiutil attach $dirname-rw.dmg
 #
 # Copy the files, overwriting placeholders
 #
-cp -r "$plugin_srcdir" $dirname
+cp -r "$plugin_srcdir"/ /Volumes/npambulant-plugin/npambulant.plugin
+cp npambulant-installer-README /Volumes/npambulant-plugin/README
+cp npambulant-installer-bg.png /Volumes/npambulant-plugin/.background/background.png
 #
-# Create temporary disk image (.dmg) and mount it
+# Done, detach and compress the new disk
 #
-hdiutil create -srcfolder $dirname $dirname-rw.dmg -format UDRW -attach
-#
-# Modify the appearance of the new disk (size, background image)
-#
-osascript - << END-OF-SCRIPT
-tell application "Finder"
-    tell disk "$dirname"
-	open 
-        tell container window
-            set current view to icon view
-            set toolbar visible to false
-            set statusbar visible to false
-            set x to 100
-            set y to 100
-            set w to 640
-            set h to 360
-            set bounds to {x, y, x + w, y + h}
-        end tell
-	set opts to the icon view options of container window
-     	tell opts
-       	    set icon size to 64
-            set shows icon preview to false
-            set shows item info to false
-            set icon size to 64
-            set text size to 16
-            set arrangement to not arranged
-            set label position to bottom
-     	end tell
-       	set background picture of opts to file ".folderbg:folderbg.png"		
-		update
-   end tell
-end tell
-END-OF-SCRIPT
-#
-# Done, unmount and compress the new disk
-#
-hdiutil detach /Volumes/$dirname
+hdiutil detach /Volumes/npambulant-plugin
+rm $dirname.dmg
 sleep 5
 hdiutil convert $dirname-rw.dmg -format UDZO -o $dirname.dmg
 #
 # Cleanup
 #
 rm $dirname-rw.dmg
-rm -fr $dirname
+
