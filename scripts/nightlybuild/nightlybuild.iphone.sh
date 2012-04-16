fa9a80e3e53bff8796893f83a7abc0c5c8e64ba4
Jack Jansen <Jack.Jansen@cwi.nl>
2012-04-16 09:22:51 +0200

    Typo

--- scripts/nightlybuild/nightlybuild.iphone.sh
+++ scripts/nightlybuild/nightlybuild.iphone.sh
@@ -8,7 +8,7 @@ set -x
 PATH=$PATH:/Developer/usr/bin
 
 # Unlock the nightly build keychain
-security unlock-keychain -p ambulant $HOME/Library/Keychains/nightlybuild.keychain
+security unlock-keychain -p ambulant $HOME/Library/Keychains/nightlybuilds.keychain
 
 # An optional parameter is the branch name, which also sets destination directory
 BRANCH=
