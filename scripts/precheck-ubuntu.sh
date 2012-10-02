3b599164a1be3bcb67a4ae02e553094db11d5089
Jack Jansen <Jack.Jansen@cwi.nl>
2012-10-02 11:26:20 +0200

    Script to check debian packages dependencies

---
+++ scripts/precheck-ubuntu.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+#
+# Check that all required development packages are
+# available, or install them if the optional "install"
+# argument is given
+packages="
+  mercurial
+  chrpath
+  autogen
+  autoconf
+  automake
+  libtool
+  g++
+  yasm
+  gettext
+  libpango1.0-dev
+  libgtk2.0-dev
+  libqt3-mt-dev
+  postfix
+  mailutils
+  curl
+  ssh
+  libxt-dev
+  libxext-dev
+  libsdl1.2-dev
+"
+debpackages="
+  devscripts
+  mercurial
+  postfix
+  mailutils
+  libxml2-dev 
+  libqt3-mt-dev 
+  liblivemedia-dev 
+  libsdl1.2-dev 
+  libavformat-dev 
+  libavcodec-dev 
+  libswscale-dev 
+  libxerces-c-dev 
+  python-dev 
+  python-gtk2-dev 
+  python-gobject-dev
+"
+
+case x$1 in
+x)
+	command="dpkg --get-selections"
+	;;
+xcheck)
+	command="dpkg --get-selections"
+	;;
+xinstall)
+	command="apt-get install"
+	;;
+xdebcheck)
+	command="dpkg --get-selections"
+	packages="$packages $debpackages"
+	;;
+xdebinstall)
+	command="apt-get install"
+	packages="$packages $debpackages"
+	;;
+*)
+	echo $0: Command can be check, install, debcheck or debinstall.
+	exit 1
+esac
+set -x
+for pkg in $packages; do
+	$command $pkg
+done
