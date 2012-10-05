6871e8e5709188ce76b948f72bad81b552425dbe
Jack Jansen <Jack.Jansen@cwi.nl>
2012-10-05 17:59:07 +0200

    Another reference to live555 libs cleared

--- scripts/merge_iphone-3pp.sh
+++ scripts/merge_iphone-3pp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ALLLIBS="./installed/lib/libavcodec.a ./installed/lib/libavdevice.a ./installed/lib/libavformat.a 	./installed/lib/libavutil.a 	./installed/lib/libexpat.a 	./installed/lib/libfaad.a 	./installed/lib/libmp4ff.a 	./installed/lib/libSDL.a 	./installed/lib/libswscale.a 	./installed/lib/libxml2.a 	./live/BasicUsageEnvironment/libBasicUsageEnvironment.a 	./live/groupsock/libgroupsock.a 	./live/liveMedia/libliveMedia.a 	./live/UsageEnvironment/libUsageEnvironment.a"
+ALLLIBS="./installed/lib/libavcodec.a ./installed/lib/libavdevice.a ./installed/lib/libavformat.a 	./installed/lib/libavutil.a 	./installed/lib/libexpat.a 	./installed/lib/libfaad.a 	./installed/lib/libmp4ff.a 	./installed/lib/libSDL.a 	./installed/lib/libswscale.a 	./installed/lib/libxml2.a"
 if [ ! -d build-iOS -o ! -d build-iOS-Simulator ]; then
 	echo Please run only in ambulant root where build-iOS and build-iOS-Simulator are located
 	exit 1
