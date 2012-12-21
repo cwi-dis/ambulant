e0f0bbbc06e8b7de5b6b55563d0356fff4c707ed
Jack Jansen <Jack.Jansen@cwi.nl>
2012-12-21 11:11:40 +0100

    New movie with 10 frames per image

--- sandbox/synctestdoc/genmovie.py
+++ sandbox/synctestdoc/genmovie.py
@@ -1,16 +1,29 @@
 from PyQRNative import *
 import os
+from PIL import ImageDraw
+
+MINUTES=4
+NFRAMESSAME=10
 
 os.mkdir('movie-tmp')
 filename = 'movie-tmp/img%06d.png'
-for i in range(6000):
-    qr = QRCode(3, QRErrorCorrectLevel.L)
-    qr.addData("%d" % i)
-    qr.make()
+lastdata=None
+for i in range(MINUTES*60*30):
+    data = int(i/NFRAMESSAME)*NFRAMESSAME
+    if data != lastdata:
+        lastdata = data
+        qr = QRCode(3, QRErrorCorrectLevel.L)
+        qr.addData("%d" % data)
+        qr.make()
 
-    im = qr.makeImage()
+        im = qr.makeImage()
+    
+        d = ImageDraw.Draw(im)
+        d.text((180, 15), "%d" % data, "red")
+        del d
     
     im.save(filename % i)
-os.system("ffmpeg -i movie-tmp/img%06d.png -vcodec libx264 -b 1000000 -vpre normal movie.m4v")
+    
+os.system("ffmpeg -i movie-tmp/img%06d.png -vcodec libx264 -b 1000000 -vpre normal qrcode-movie.mp4")
 
     
