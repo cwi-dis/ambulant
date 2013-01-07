090bdf2fb02493608cef3e3dd6933339231a1bc2
Jack Jansen <Jack.Jansen@cwi.nl>
2013-01-07 14:34:04 +0100

    Upped plugin version

--- include/ambulant/common/plugin_engine.h
+++ include/ambulant/common/plugin_engine.h
@@ -25,7 +25,7 @@
 #include "ambulant/common/factory.h"
 #include "ambulant/common/gui_player.h"
 
-#define AMBULANT_PLUGIN_API_VERSION 9
+#define AMBULANT_PLUGIN_API_VERSION 10
 
 namespace ambulant {
 
