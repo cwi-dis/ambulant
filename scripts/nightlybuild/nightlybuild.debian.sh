2bd810209737e67fd210a84ca6196c0f0d472c51
Jack Jansen <Jack.Jansen@cwi.nl>
2013-01-11 16:16:23 +0100

    Build binary and source packages separately

--- scripts/nightlybuild/nightlybuild.debian.sh
+++ scripts/nightlybuild/nightlybuild.debian.sh
@@ -59,10 +59,12 @@ x)
 xrelease*)
 	DESTINATION=$DESTINATION/$BRANCH
 	VERSIONSUFFIX=
+	release=yes
 	;;
 *)
 	DESTINATION=$DESTINATION/$BRANCH
 	VERSIONSUFFIX=.$TODAY
+	release=no
 esac
 CLDATE=`date --rfc-2822`
 BUILDDIR=ambulant-debian-$TODAY
@@ -110,6 +112,8 @@ rm -r .hg
 rm -r sandbox
 
 sh autogen.sh
+case x$release in
+xno)
 cat > debian/changelog << xyzzy
 ambulant ($AMBULANTVERSION$VERSIONSUFFIX) unstable; urgency=low
 
@@ -117,27 +121,35 @@ ambulant ($AMBULANTVERSION$VERSIONSUFFIX) unstable; urgency=low
 
  -- CWI Ambulant Team <ambulant@cwi.nl>  $CLDATE
 xyzzy
+;;
+esac
 
 #
-# Build debian package (incomplete)
+# Build debian packages, first binary then source
 #
+rm -rf $DESTINATION_STAGING
+mkdir -p $RELPATH_SRC/debian-$TODAY
+mkdir -p $RELPATH_BIN/debian-$TODAY
+
 cd debian
-debuild --no-tgz-check -kC75B80BC 
+debuild -kC75B80BC 
 cd ..
+mv *.deb *.dsc *.changes $RELPATH_BIN/debian-$TODAY/
+
+cd debian
+debuild -S -sa -kC75B80BC 
+cd ..
+mv *.tar.gz *.dsc *.changes *.build $RELPATH_SRC/debian-$TODAY/
+
+dpkg-scanpackages $RELPATH_BIN/debian-$TODAY | gzip -9c > $RELPATH_BIN/Packages.gz
+dpkg-scansources $RELPATH_SRC/debian-$TODAY | gzip -9c > $RELPATH_SRC/Sources.gz
+rsync -r $DESTINATION_STAGING $DESTINATION_DEBIAN
 
 #
 # Upload
 #
 
 cd ..
-rm -rf $DESTINATION_STAGING
-mkdir -p $RELPATH_SRC/debian-$TODAY
-mkdir -p $RELPATH_BIN/debian-$TODAY
-mv *.tar.gz *.dsc *.changes *.build $RELPATH_SRC/debian-$TODAY/
-mv *.deb $RELPATH_BIN/debian-$TODAY/
-dpkg-scanpackages $RELPATH_BIN/debian-$TODAY | gzip -9c > $RELPATH_BIN/Packages.gz
-dpkg-scansources $RELPATH_SRC/debian-$TODAY | gzip -9c > $RELPATH_SRC/Sources.gz
-rsync -r $DESTINATION_STAGING $DESTINATION_DEBIAN
 
 #
 # Delete old installers, remember current
