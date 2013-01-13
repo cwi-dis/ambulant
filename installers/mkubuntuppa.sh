8de6c072d7d58562645c1914010911995a655284
Jack Jansen <Jack.Jansen@cwi.nl>
2013-01-13 22:36:00 +0100

    Different scripts for Ubuntu PPA and debian binary packages

--- installers/mkubuntuppa.sh
+++ installers/mkubuntuppa.sh
@@ -1,37 +1,54 @@
 #!/bin/sh
 set -e -x
-source=false
 VERSION=2.4.1
+UVERSION=precise
 if [ ! -f include/ambulant/version.h ]; then
 	echo "Please run only in a toplevel ambulant directory"
 	exit 1
 fi
-echo '+ May need: sudo sudo apt-get install dpkg-dev debhelper devscripts fakeroot'
+case x$1 in
+xrelease)
+	;;
+xnightly)
+	VERSION=$VERSION.`date +%Y%m%d`
+	fixchangelog=yes
+	;;
+*)
+	echo Usage: $0 [release|nightly] [branch]
+	exit 1
+esac
+
 if [ -d ambulant-debiandist-tmp ]; then
 	rm -rf ambulant-debiandist-tmp
 fi
 mkdir ambulant-debiandist-tmp
 cd ambulant-debiandist-tmp
-case x$1 in
+case x$2 in
 x)	branch=default
 	;;
-*)	branch=$1
+*)	branch=$2
 	;;
 esac
 echo Building debian packages for ambulant-$VERSION from branch $branch
 hg clone -u $branch .. ambulant-$VERSION
 rm -rf ambulant-$VERSION/.hg
 rm -rf ambulant-$VERSION/sandbox
+case x$fixchangelog in
+xyes)
+	CLDATE=`date --rfc-2822`
+	cat > debian/changelog << xyzzy
+ambulant ($VERSION) $UVERSION; urgency=low
+
+  * Nightly build, for testing only
+
+ -- CWI Ambulant Team <ambulant@cwi.nl>  $CLDATE
+xyzzy
+	;;
+esac
 tar cfz ambulant_$VERSION.orig.tar.gz ambulant-$VERSION
 cd ambulant-$VERSION
-if $source; then
-	debuild -S -sa -kC75B80BC
-else
-	debuild -kC75B80BC
-fi
+debuild -S -sa -kC75B80BC
 cd ..
 rm -rf ambulant-$VERSION
-dpkg-scanpackages . /dev/null | gzip -9c >Packages.gz
-dpkg-scansources . /dev/null | gzip -9c > Sources.gz
 
 
