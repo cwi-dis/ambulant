21ca52795c6bd323cf063ce5037ac494102fb5f3
Jack Jansen <Jack.Jansen@cwi.nl>
2013-01-13 12:48:50 +0100

    Another dependency added

--- installers/mkdebiandist.sh
+++ installers/mkdebiandist.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e -x
 source=true
-VERSION=2.4.1.20130112
+VERSION=2.4.1.20130113
 if [ ! -f include/ambulant/version.h ]; then
 	echo "Please run only in a toplevel ambulant directory"
 	exit 1
