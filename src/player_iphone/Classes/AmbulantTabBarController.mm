8df8b85e146f1b44266e300f31b42eb427776188
Kees Blom <kees@cwi.nl>
2013-01-17 13:49:55 +0100

    Added AmbulantTabBarController as a subclass of UITabBarController, referred to in MainWindow.xib to make auto rotate work.

---
+++ src/player_iphone/Classes/AmbulantTabBarController.mm
@@ -0,0 +1,64 @@
+// This file is part of Ambulant Player, www.ambulantplayer.org.
+//
+// Copyright (C) 2003-2012 Stichting CWI,
+// Science Park 123, 1098 XG Amsterdam, The Netherlands.
+//
+// Ambulant Player is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// Ambulant Player is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Ambulant Player; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+//  AmbulantTabBarController.m
+//  iAmbulant
+//
+//  Created by Kees Blom on 1/17/13.
+//
+//
+
+#import "AmbulantTabBarController.h"
+
+@interface AmbulantTabBarController ()
+
+@end
+
+@implementation AmbulantTabBarController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)shouldAutorotate { // iOS >= 6
+    return YES;
+}
+
+- (NSUInteger)supportedInterfaceOrientations { // iOS >= 6
+    return UIInterfaceOrientationMaskAll;
+}
+
+@end
