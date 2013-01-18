a7e059b29073aa3711928ed78dde18354d848b64
Kees Blom <kees@cwi.nl>
2013-01-18 10:31:47 +0100

    Added some missing tools ins PATH (hack)

--- scripts/nightlybuild/nightlybuild.iphone.sh
+++ scripts/nightlybuild/nightlybuild.iphone.sh
@@ -6,6 +6,7 @@
 set -e
 set -x
 export PATH=`which xcode-select -print-path`/usr/local/bin:/Developer/usr/bin:$PATH
+PATH=$PATH:/Users/kees/bin # Hack for missing stuff
 
 # Unlock the nightly build keychain
 security unlock-keychain -p ambulant $HOME/Library/Keychains/nightlybuilds.keychain
