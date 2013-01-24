d5bf5e22fa0f9ed13ba28caacd982ce090a35439
Jack Jansen <Jack.Jansen@cwi.nl>
2013-01-24 23:22:03 +0100

    CGRect and NSRect are not identical in 32bit builds

--- scripts/ffmpeg-osx-fatbuild.sh
+++ scripts/ffmpeg-osx-fatbuild.sh
@@ -17,23 +17,15 @@ PREFIX="--prefix=`cd ../installed ; pwd`"
 #
 # Set this to the global options you want to configure ffmpeg with.
 #
-#CONFIGOPTS="$PREFIX --disable-encoders --enable-swscale --enable-gpl --disable-vhook --disable-ffserver --disable-ffmpeg --disable-ffplay --disable-ffprobe --enable-static --enable-shared --enable-libfaad --disable-libfaac"
 CONFIGOPTS="$PREFIX --disable-encoders --enable-runtime-cpudetect --enable-gpl --disable-ffserver --disable-ffmpeg --disable-ffplay --disable-ffprobe --disable-yasm --enable-static --enable-shared"
 #
 # If you want to build for a different MacOSX version than the current one
 # define SYSROOT and MACOSX_DEPLOYMENT_TARGET
 #
-###SYSROOT=" -isysroot /Developer/SDKs/MacOSX10.4u.sdk"
-##export MACOSX_DEPLOYMENT_TARGET=10.4
-##CONFIGOPTS="--cc=gcc-4.0  --extra-cflags=-I`cd ../installed/include; pwd` --extra-ldflags=-L`cd ../installed/lib; pwd` "$CONFIGOPTS
-SYSROOT=" -isysroot $SDK_PATH"
-CONFIGOPTS="--extra-cflags=-I`cd ../installed/include; pwd` --extra-ldflags=-L`cd ../installed/lib; pwd` "$CONFIGOPTS
 #
 # Set variables here to true to include the ABI
 #
-PPC=false
 I386=true
-PPC64=false
 X86_64=true
 #
 # Set this variable to any of the architectures you are build
@@ -43,15 +35,13 @@ ANY_RANDOM_ARCH=i386
 #
 # You may need to fiddle these, but probably not.
 #
-PPC_CONFIGOPTS="--arch=powerpc --cpu=g4 --enable-altivec --enable-shared"
 case `uname -r` in
 11.*)
 	I386_CONFIGOPTS="--arch=i386 --cpu=i686 --enable-shared --cc=clang --disable-mmx"
 	;;
 *)
-	I386_CONFIGOPTS="--arch=i686 --cpu=i686 --enable-shared --cc=gcc-4.0"
+	I386_CONFIGOPTS="--arch=i686 --cpu=i686 --enable-shared"
 esac
-PPC64_CONFIGOPTS="--enable-cross-compile --arch=ppc64 --cpu=g4 --enable-altivec --enable-shared"
 X86_64_CONFIGOPTS="--arch=x86_64 --enable-shared"
 #
 # Should be boilerplate from here on
@@ -114,9 +104,7 @@ done
 if $mkdirs; then
     echo $0: mkdirs
     if $I386; then mkdir build-i386; fi
-    if $PPC; then mkdir build-ppc; fi
     if $X86_64; then mkdir build-x86_64; fi
-    if $PPC64; then mkdir build-ppc64; fi
     mkdir libavformat
     mkdir libavcodec
     mkdir libavutil
@@ -140,54 +128,25 @@ fi
 if $configure; then
 	if $I386; then
 		echo $0: configure i386
-		(
-			set -vx
-			cd build-i386
-			$srcdir/configure \
-				--extra-cflags="-arch i386 $SYSROOT" \
-				--extra-ldflags="-arch i386 $SYSROOT" \
-				$I386_CONFIGOPTS \
-				$CONFIGOPTS
-		)
+		cd build-i386
+		$srcdir/configure \
+			--extra-cflags="-arch i386 $SYSROOT" \
+			--extra-ldflags="-arch i386 $SYSROOT" \
+			$I386_CONFIGOPTS \
+			$CONFIGOPTS
+		cd ..
 	else
 		echo $0: skipping configure for i386
 	fi
-	if $PPC; then
-		echo $0: configure ppc
-		(
-			cd build-ppc
-			$srcdir/configure \
-				--extra-cflags="-arch ppc $SYSROOT" \
-				--extra-ldflags="-arch ppc $SYSROOT" \
-				$PPC_CONFIGOPTS \
-				$CONFIGOPTS
-		)
-	else
-		echo $0: skipping configure for ppc
-	fi
-	if $PPC64; then
-		echo $0: configure ppc64
-		(
-			cd build-ppc64
-			$srcdir/configure \
-				--extra-cflags="-arch ppc64 $SYSROOT" \
-				--extra-ldflags="-arch ppc64 $SYSROOT" \
-				$PPC64_CONFIGOPTS \
-				$CONFIGOPTS
-		)
-	else
-		echo $0: skipping configure for ppc64
-	fi
 	if $X86_64; then
 		echo $0: configure x86_64
-		(
-			cd build-x86_64
-			$srcdir/configure \
-				--extra-cflags="-arch x86_64 $SYSROOT" \
-				--extra-ldflags="-arch x86_64 $SYSROOT" \
-				$X86_64_CONFIGOPTS \
-				$CONFIGOPTS
-		)
+		cd build-x86_64
+		$srcdir/configure \
+			--extra-cflags="-arch x86_64 $SYSROOT" \
+			--extra-ldflags="-arch x86_64 $SYSROOT" \
+			$X86_64_CONFIGOPTS \
+			$CONFIGOPTS
+		cd ..
 	else
 		echo $0: skipping configure for x86_64
 	fi
@@ -198,19 +157,15 @@ fi
 if $clean; then
 	if $I386; then
 		echo $0: clean i386
-		( cd build-i386; make clean)
-	fi
-	if $PPC; then
-		echo $0: clean ppc
-		( cd build-ppc; make clean)
-	fi
-	if $PPC64; then
-		echo $0: clean ppc64
-		( cd build-ppc64; make clean)
+		cd build-i386
+		make clean
+		cd ..
 	fi
 	if $X86_64; then
 		echo $0: clean x86_64
-		( cd build-x86_64; make clean)
+		cd build-x86_64
+		make clean
+		cd ..
 	fi
 else
     echo $0: skipping clean
@@ -219,25 +174,17 @@ fi
 if $build; then
 	if $I386; then
 		echo $0: build i386
-		( cd build-i386; make $MAKEFLAGS)
+		cd build-i386
+		make $MAKEFLAGS
+		cd ..
 	else
 		echo $0: skipping build for i386
 	fi
-	if $PPC; then
-		echo $0: build ppc
-		( cd build-ppc; make $MAKEFLAGS)
-	else
-		echo $0: skipping build for ppc
-	fi
-	if $PPC64; then
-		echo $0: build ppc64
-		( cd build-ppc64; make $MAKEFLAGS)
-	else
-		echo $0: skipping build for ppc64
-	fi
 	if $X86_64; then
 		echo $0: build x86_64
-		( cd build-x86_64; make $MAKEFLAGS)
+		cd build-x86_64
+		make $MAKEFLAGS
+		cd ..
 	else
 		echo $0: skipping build for x86_64
 	fi
@@ -271,7 +218,9 @@ fi
 
 if $install; then
     echo $0: install
-    (cd build-$ANY_RANDOM_ARCH ; make -n install) | sed -e 's/^.*$/(&)/' > install.sh
+    cd build-$ANY_RANDOM_ARCH
+    make -n install | sed -e 's/^.*$/(&)/' > ../install.sh
+    cd ..
     sh install.sh
 else
     echo $0: skipping install
