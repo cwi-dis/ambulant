73c5464e36b1d421b18772c3ee490870efb16b83
Kees Blom <kees@cwi.nl>
2013-04-22 16:30:52 +0200

    Added an early check whether SDL2 and SDL_image can work

--- src/player_sdl/sdl_gui.cpp
+++ src/player_sdl/sdl_gui.cpp
@@ -775,9 +775,15 @@ sdl_gui::sdl_loop() {
 int
 main (int argc, char*argv[]) {
 
-	SDL_Init(SDL_INIT_EVERYTHING);
-	IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF | IMG_INIT_WEBP);
-
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0 ) {
+		fprintf (stderr, "SDL_Init failed: %s\n", SDL_GetError());
+		exit (-1);
+	}
+	int img_flags = IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF | IMG_INIT_WEBP; 
+	if (IMG_Init(img_flags) != img_flags) {
+		fprintf (stderr, "IMG_Init failed.\n");
+		exit (-2);
+	}
 #ifdef	ENABLE_NLS
 	// Load localisation database
 	bool private_locale = false;
