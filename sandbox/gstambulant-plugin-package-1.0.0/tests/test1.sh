842fd531666108c989e050a222b407ce8dcff5a4
Kees Blom <kees@cwi.nl>
2013-04-29 16:18:39 +0200

    Adapted test cases for ambulantsrc Gstreamer plugin to handle the changed pixel format

--- sandbox/gstambulant-plugin-package-1.0.0/tests/test1.sh
+++ sandbox/gstambulant-plugin-package-1.0.0/tests/test1.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 # set -x
-if  xvinfo|grep "no adaptors"  ; then echo "\n*** No XVideo available, maybe nothing to see. ***\n"; fi
 echo GST_PLUGIN_PATH=$GST_PLUGIN_PATH # change this appropriately when a --prefix 'configure' option value is use
-# TBD get the caps from tests/input and use these in the sink pad of the plugin
-echo $srcdir
-gst-launch-1.0 --gst-debug-level=2 ambulantsrc ! video/x-raw,format=RGB,width=300,height=260,bpp=24,depth=24,framerate=30/1,endianness=4321,pixel-aspect-ratio=5/4,red_mask=16711680,green_mask=65280,blue_mask=255 ! videoconvert ! sdlvideosink < $srcdir/input
-if  [ $? ]
-then   gst-launch-1.0 --gst-debug-level=2 ambulantsrc ! video/x-raw,format=RGB,width=300,height=260,bpp=24,depth=24,framerate=30/1,endianness=4321,pixel-aspect-ratio=5/4,red_mask=16711680,green_mask=65280,blue_mask=255 ! videoconvert ! autovideosink < $srcdir/input
-fi
-if  [ $? ]
-then   gst-launch-1.0 --gst-debug-level=2 ambulantsrc ! video/x-raw,format=RGB,width=300,height=260,bpp=24,depth=24,framerate=30/1,endianness=4321,pixel-aspect-ratio=5/4,red_mask=16711680,green_mask=65280,blue_mask=255 ! videoscale ! videoconvert ! ximagesink < $srcdir/input
-fi
+echo "Using ximagesink"
+gst-launch-1.0 --gst-debug-level=2 ambulantsrc ! video/x-raw,format=BGRA,width=300,height=260,bpp=32,depth=32,framerate=30/1,endianness=4321,pixel-aspect-ratio=1/1,green_mask=16711680,red_mask=65280 ! videoconvert ! videoscale ! ximagesink < $srcdir/input
+
