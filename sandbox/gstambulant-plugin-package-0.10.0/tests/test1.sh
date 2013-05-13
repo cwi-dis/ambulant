4373ba5be9d0a8f70556778bc04208be2daa9422
Kees Blom <kees@cwi.nl>
2013-05-13 13:37:10 +0200

    Simplified code s.t. 'make check' find the plugin at the spot they were build

--- sandbox/gstambulant-plugin-package-0.10.0/tests/test1.sh
+++ sandbox/gstambulant-plugin-package-0.10.0/tests/test1.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # set -x
-GST_PLUGIN_PATH=/usr/local/lib/gstreamer-0.10 # change this appropriately when a --prefix 'configure' option value is used
 # TBD get the caps from tests/input and use these in the sink pad of the plugin
 echo $srcdir
 gst-launch --gst-debug-level=2 ambulantsrc  ! ffmpegcolorspace ! sdlvideosink < $srcdir/input
