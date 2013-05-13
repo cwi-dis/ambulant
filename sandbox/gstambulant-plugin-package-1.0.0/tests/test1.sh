ef91d3e10ae8951a6216a54f024edb0359e174fd
Kees Blom <kees@cwi.nl>
2013-05-13 13:36:37 +0200

    Simplified code s.t. 'make check' find the plugin at the spot they were build

--- sandbox/gstambulant-plugin-package-1.0.0/tests/test1.sh
+++ sandbox/gstambulant-plugin-package-1.0.0/tests/test1.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 # set -x
-#export GST_PLUGIN_PATH=../src/.libs
-echo GST_PLUGIN_PATH=$GST_PLUGIN_PATH # change this appropriately when a --prefix 'configure' option value is use
 echo "Using ximagesink"
-gst-launch-1.0 --gst-debug-level=1 ambulantsrc silent=0 ! videoconvert ! videoscale ! ximagesink < $srcdir/input&
-PID=$?
-#sleep 5
-#kill -9 $PID
+gst-launch-1.0 ambulantsrc silent=1 ! videoconvert ! videoscale ! ximagesink < $srcdir/input
+
 
