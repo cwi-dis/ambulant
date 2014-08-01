59006d3b81bfed27f491ae6e18ae519db271e33a
Kees Blom <kees@cwi.nl>
2014-08-01 21:52:19 +0200

    Removed --with-sdl2 (accidental committed earlier)

--- scripts/nightlybuild/nightlybuild.mac.sh
+++ scripts/nightlybuild/nightlybuild.mac.sh
@@ -45,7 +45,7 @@ esac
 BUILDDIR=ambulant-build-$TODAY
 DESTDIR=ambulant-install-$TODAY
 BUILD3PPARGS=macosx$MACOSX_DEPLOYMENT_TARGET
-CONFIGOPTS="--with-macfat --disable-dependency-tracking --with-xerces-plugin --with-python=/usr/bin/python --with-python-plugin --with-included-ltdl --with-sdl2"
+CONFIGOPTS="--with-macfat --disable-dependency-tracking --with-xerces-plugin --with-python=/usr/bin/python --with-python-plugin --with-included-ltdl"
 DMGNAME=Ambulant-$AMBULANTVERSION$VERSIONSUFFIX-mac
 PLUGINNAME=npambulant-$AMBULANTVERSION$VERSIONSUFFIX-mac
 PLUGINDMGNAME=$PLUGINNAME.dmg
