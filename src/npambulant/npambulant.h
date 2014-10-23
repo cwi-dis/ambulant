814d0b9fb33bf9aa3aee19cd30f7b6d25c6c070c
Kees Blom <Kees.Blom.cwi.nl>
2014-10-23 23:44:05 +0200

    Fixed 'empty format string' warnings

--- src/npambulant/npambulant.h
+++ src/npambulant/npambulant.h
@@ -257,7 +257,7 @@ void npambulant_display_message(int level, const char *message);
 #ifndef	XP_WIN32
 #define LOG(formatAndArgs...) AM_DBG { \
 					fprintf (stderr, "%s(%p):  ", __PRETTY_FUNCTION__, this); \
-					fprintf(stderr, formatAndArgs); fprintf(stderr, "\n");	\
+					fprintf(stderr, formatAndArgs); fprintf(stderr, "\n"); \
 					}
 #else
 #define LOG AM_DBG ambulant::lib::logger::get_logger()->debug
