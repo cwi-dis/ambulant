8cdcdc4e2f9cba1317ebbf606580d35a217325ce
Kees Blom <Kees.Blom.cwi.nl>
2014-12-22 11:58:49 +0100

    Added a dummy file 'xerces_parser.cpp' to avoid problems in 'make distcheck'

---
+++ src/plugins/xerces_parser.cpp
@@ -0,0 +1,4 @@
+/* This file is hack to use a source file from libambulant in our plugin 
+ * without getting problems problems in 'make distcheck', used in nightlybuilds.
+ */ 
+#include "../../src/libambulant/lib/xerces_parser.cpp"
