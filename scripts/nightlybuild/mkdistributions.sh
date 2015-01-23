56c02f7a7f35bd719caad23b6054117b1f75c94a
Jack Jansen <Jack.Jansen@cwi.nl>
2015-01-23 15:05:52 +0100

    Fixes

--- scripts/nightlybuild/mkdistributions.sh
+++ scripts/nightlybuild/mkdistributions.sh
@@ -33,7 +33,7 @@ mkdistribution() {
 	scriptname=nightlybuild.${distribution}.sh
 	curl  -o /tmp/${scriptname} http://ambulantplayer.org/cgi-bin/hgweb.cgi/hg/ambulant/raw-file/${BRANCHNAME}/scripts/nightlybuild/${scriptname}
 	scp /tmp/${scriptname} ${hostname}:/tmp/${scriptname}
-	ssh ${hostname} /tmp/${scriptname} ${BRANCHNAME}
+	ssh ${hostname} sh /tmp/${scriptname} ${BRANCHNAME}
 }
 
 PHOST=Mac-1010-Nightly
